03f9cb5888c1ea04151a8398d0e9e08e774eb75a
Jamie Brandon <jamie@scattered-thoughts.net>
2015-09-02 13:24:39 -0700

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate test;
 extern crate time;
 
 use rand::{Rng, SeedableRng, StdRng};
+use std::cmp::{max, Ordering};
 
 macro_rules! time {
     ($name:expr, $expr:expr) => {{
@@ -17,17 +18,15 @@ macro_rules! time {
     }};
 }
 
+pub fn ids(seed: usize) -> Vec<Id> {
+    let mut rng = StdRng::new().unwrap();
+    rng.reseed(&[seed+0, seed+1, seed+2, seed+3]);
+    (0..100_000).map(|_| rng.gen_range(0, 100_000)).collect()
+}
+
 pub type Id = u32;
 
 fn main() {
-    fn ids(seed: usize) -> Vec<u32> {
-        let n = 1<<29;
-        let mut rng = StdRng::new().unwrap();
-        rng.reseed(&[seed+0, seed+1, seed+2, seed+3]);
-        (0..n)
-        .map(|_| rng.gen_range(0,n))
-        .collect()
-    }
     let mut ids = test::black_box(ids(4));
     time!("radix sort on random", {
         radix_sort(&mut ids);
@@ -47,23 +46,49 @@ fn get_byte(id: Id, offset: usize) -> usize {
 pub fn radix_sort(ids: &mut Vec<Id>) {
     let mut buffer = ids.clone();
     for offset in (0..4) {
-        {
-            let mut counts = [0; 256];
-            for id in ids.iter() {
-                counts[get_byte(*id, offset)] += 1;
-            }
-            let mut buckets = [0; 256];
-            for ix in (1..256) {
-                buckets[ix] = buckets[ix-1] + counts[ix-1];
-            }
-            for id in ids.iter() {
-                let byte = get_byte(*id, offset);
-                buffer[buckets[byte]] = *id;
-                buckets[byte] += 1;
-            }
+        let mut counts = [0; 256];
+        for id in ids.iter() {
+            counts[get_byte(*id, offset)] += 1;
+        }
+        let mut buckets = [0; 256];
+        for ix in (1..256) {
+            buckets[ix] = buckets[ix-1] + counts[ix-1];
+        }
+        for id in ids.iter() {
+            let byte = get_byte(*id, offset);
+            buffer[buckets[byte]] = *id;
+            buckets[byte] += 1;
         }
         ::std::mem::swap(&mut buffer, ids);
     }
+}
+
+pub fn intersect_sorted(ids_a: &Vec<Id>, ids_b: &Vec<Id>) -> Vec<Id> {
+    let mut results = Vec::with_capacity(max(ids_a.len(), ids_b.len()));
+    let mut ix_a = 0;
+    let mut ix_b = 0;
+    loop {
+        match (ids_a.get(ix_a), ids_b.get(ix_b)) {
+            (Some(&a), Some(&b)) => {
+                match a.cmp(&b) {
+                    Ordering::Less => {
+                        ix_a += 1;
+                    }
+                    Ordering::Equal => {
+                        // a real join would have to have a nested loop here to deal with dupes
+                        results.push(a);
+                        ix_a += 1;
+                        ix_b += 1;
+                    }
+                    Ordering::Greater => {
+                        ix_b += 1;
+                    }
+                }
+            }
+            _ => break,
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -72,21 +97,9 @@ mod tests {
 
     use std::collections::HashSet;
     use std::collections::BTreeSet;
-    use std::cmp::{max, Ordering};
+    use std::cmp::max;
 
-    use rand::{Rng, SeedableRng, StdRng};
     use test::{Bencher, black_box};
-
-    fn ids(seed: usize) -> Vec<u32> {
-        let n = 100_000;
-        let mut rng = StdRng::new().unwrap();
-        rng.reseed(&[seed+0, seed+1, seed+2, seed+3]);
-        let mut seen_ids = HashSet::new();
-        (0..n)
-            .map(|_| rng.gen_range(0,n))
-            .filter(|id| seen_ids.insert(*id)) // filter out dupes
-            .collect()
-    }
 
     #[test]
     fn test_radix_sort() {
@@ -188,16 +201,16 @@ mod tests {
     }
 
     #[bench]
-    fn bench_small_btree_intersect(bencher: &mut Bencher) {
+    fn bench_hash_index_intersect(bencher: &mut Bencher) {
         let ids_a = black_box(ids(7));
         let ids_b = black_box(ids(42));
         let max_size = max(ids_a.len(), ids_b.len());
+        let mut index = HashSet::with_capacity(max_size);
+        for id in ids_a.iter() {
+            index.insert(*id);
+        }
         bencher.iter(|| {
             let mut results = Vec::with_capacity(max_size);
-            let mut index = BTreeSet::with_b(10);
-            for id in ids_a.iter() {
-                index.insert(*id);
-            }
             for id in ids_b.iter() {
                 if index.contains(id) {
                     results.push(*id);
@@ -207,61 +220,69 @@ mod tests {
         });
     }
 
-    #[bench]
-    fn bench_large_btree_intersect(bencher: &mut Bencher) {
-        let ids_a = black_box(ids(7));
-        let ids_b = black_box(ids(42));
-        let max_size = max(ids_a.len(), ids_b.len());
-        bencher.iter(|| {
-            let mut results = Vec::with_capacity(max_size);
-            let mut index = BTreeSet::with_b(100);
-            for id in ids_a.iter() {
-                index.insert(*id);
-            }
-            for id in ids_b.iter() {
-                if index.contains(id) {
-                    results.push(*id);
-                }
-            }
-            black_box(results);
-        });
-    }
+    // #[bench]
+    // fn bench_small_btree_intersect(bencher: &mut Bencher) {
+    //     let ids_a = black_box(ids(7));
+    //     let ids_b = black_box(ids(42));
+    //     let max_size = max(ids_a.len(), ids_b.len());
+    //     bencher.iter(|| {
+    //         let mut results = Vec::with_capacity(max_size);
+    //         let mut index = BTreeSet::with_b(10);
+    //         for id in ids_a.iter() {
+    //             index.insert(*id);
+    //         }
+    //         for id in ids_b.iter() {
+    //             if index.contains(id) {
+    //                 results.push(*id);
+    //             }
+    //         }
+    //         black_box(results);
+    //     });
+    // }
+
+    // #[bench]
+    // fn bench_large_btree_intersect(bencher: &mut Bencher) {
+    //     let ids_a = black_box(ids(7));
+    //     let ids_b = black_box(ids(42));
+    //     let max_size = max(ids_a.len(), ids_b.len());
+    //     bencher.iter(|| {
+    //         let mut results = Vec::with_capacity(max_size);
+    //         let mut index = BTreeSet::with_b(100);
+    //         for id in ids_a.iter() {
+    //             index.insert(*id);
+    //         }
+    //         for id in ids_b.iter() {
+    //             if index.contains(id) {
+    //                 results.push(*id);
+    //             }
+    //         }
+    //         black_box(results);
+    //     });
+    // }
 
     #[bench]
     fn bench_std_sort_intersect(bencher: &mut Bencher) {
         let ids_a = black_box(ids(7));
         let ids_b = black_box(ids(42));
-        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             // the clone unfairly penalises this test, since in a real use we could just sort in place
             let mut sorted_a = ids_a.clone();
             let mut sorted_b = ids_b.clone();
             sorted_a.sort();
             sorted_b.sort();
-            let mut results: Vec<u32> = Vec::with_capacity(max_size);
-            let mut ix_a = 0;
-            let mut ix_b = 0;
-            loop {
-                match (sorted_a.get(ix_a), sorted_b.get(ix_b)) {
-                    (Some(&a), Some(&b)) => {
-                        match a.cmp(&b) {
-                            Ordering::Less => {
-                                ix_a += 1;
-                            }
-                            Ordering::Equal => {
-                                // a real join would have to have a nested loop here to deal with dupes
-                                results.push(a);
-                                ix_a += 1;
-                                ix_b += 1;
-                            }
-                            Ordering::Greater => {
-                                ix_b += 1;
-                            }
-                        }
-                    }
-                    _ => break,
-                }
-            }
+            let results = intersect_sorted(&sorted_a, &sorted_b);
+            black_box(results);
+        });
+    }
+
+    #[bench]
+    fn bench_sorted_index_intersect(bencher: &mut Bencher) {
+        let mut ids_a = black_box(ids(7));
+        let mut ids_b = black_box(ids(42));
+        ids_a.sort();
+        ids_b.sort();
+        bencher.iter(|| {
+            let results = intersect_sorted(&ids_a, &ids_b);
             black_box(results);
         });
     }
@@ -270,37 +291,13 @@ mod tests {
     fn bench_radix_sort_intersect(bencher: &mut Bencher) {
         let ids_a = black_box(ids(7));
         let ids_b = black_box(ids(42));
-        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             // the clone unfairly penalises this test, since in a real use we could just sort in place
             let mut sorted_a = ids_a.clone();
             let mut sorted_b = ids_b.clone();
             radix_sort(&mut sorted_a);
             radix_sort(&mut sorted_b);
-            let mut results: Vec<u32> = Vec::with_capacity(max_size);
-            let mut ix_a = 0;
-            let mut ix_b = 0;
-            loop {
-                match (sorted_a.get(ix_a), sorted_b.get(ix_b)) {
-                    (Some(&a), Some(&b)) => {
-                        match a.cmp(&b) {
-                            Ordering::Less => {
-                                ix_a += 1;
-                            }
-                            Ordering::Equal => {
-                                // a real join would have to have a nested loop here to deal with dupes
-                                results.push(a);
-                                ix_a += 1;
-                                ix_b += 1;
-                            }
-                            Ordering::Greater => {
-                                ix_b += 1;
-                            }
-                        }
-                    }
-                    _ => break,
-                }
-            }
+            let results = intersect_sorted(&sorted_a, &sorted_b);
             black_box(results);
         });
     }

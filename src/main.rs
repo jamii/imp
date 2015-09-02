b28a912a0d1d13eb5847b86e11801e7553d94c19
Jamie Brandon <jamie@scattered-thoughts.net>
2015-09-01 22:16:02 -0700

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(test)]
-#![feature(collections)]
-#![feature(collections_drain)]
+#![feature(btree_b)]
 
 extern crate rand;
 extern crate test;
 extern crate time;
+
+use rand::{Rng, SeedableRng, StdRng};
 
 macro_rules! time {
     ($name:expr, $expr:expr) => {{
@@ -16,41 +17,58 @@ macro_rules! time {
     }};
 }
 
+pub type Id = u32;
+
 fn main() {
-    let mut nums = test::black_box((0..(1 << 27)).collect::<Vec<_>>());
-    let mut buffers = make_buffers(nums.len() / 256);
-    time!("radix sort", {
-        radix_sort(&mut nums, &mut buffers);
+    fn ids(seed: usize) -> Vec<u32> {
+        let n = 1<<27;
+        let mut rng = StdRng::new().unwrap();
+        rng.reseed(&[seed+0, seed+1, seed+2, seed+3]);
+        (0..n)
+        .map(|_| rng.gen_range(0,n))
+        .collect()
+    }
+    let mut ids = test::black_box(ids(4));
+    time!("radix sort on random", {
+        radix_sort(&mut ids);
     });
-    test::black_box(&nums);
-    println!("{:?} {:?}", nums[0], nums[nums.len()-1]);
+    test::black_box(&ids);
+    let mut ids = test::black_box((1..(1<<27)).collect());
+    time!("radix sort on sorted", {
+        radix_sort(&mut ids);
+    });
+    test::black_box(&ids);
 }
 
-fn get_byte(num: usize, offset: usize) -> usize {
-    (num >> offset) & 0b1111_1111
+fn get_byte(id: Id, offset: usize) -> usize {
+    ((id >> (offset * 8)) & 0b1111_1111) as usize
 }
 
-pub fn make_buffers(len: usize) -> Vec<Vec<usize>> {
-    (0..256).map(|_| Vec::with_capacity(len)).collect::<Vec<_>>()
-}
-
-pub fn radix_sort(nums: &mut Vec<usize>, buffers: &mut Vec<Vec<usize>>) {
-    for i in (0..8) {
-        let offset = i * 8;
-        for ix in (0..nums.len()) {
-            let num = unsafe{ *nums.get_unchecked(ix) };
-            let byte = get_byte(num, offset);
-            let buffer = unsafe{ buffers.get_unchecked_mut(byte) };
-            buffer.push(num);
-        }
-        unsafe{ nums.set_len(0); }
-        for buffer in buffers.iter_mut() {
-            for ix in (0..buffer.len()) {
-                let num = unsafe{ *buffer.get_unchecked(ix) };
-                nums.push(num);
+pub fn radix_sort(ids: &mut Vec<Id>) {
+    let mut buffer = ids.clone();
+    for offset in (0..4) {
+        {
+            let ids_inner = &mut ids[..];
+            let mut counts = [0; 256];
+            for ix in (0..ids_inner.len()) {
+                let id = unsafe{ *ids_inner.get_unchecked(ix) };
+                let byte = get_byte(id, offset);
+                let count = unsafe { counts.get_unchecked_mut(byte) };
+                *count = *count + 1;
             }
-            unsafe{ buffer.set_len(0); }
+            let mut buckets = [0; 256];
+            for ix in (1..256) {
+                unsafe { *buckets.get_unchecked_mut(ix) = *buckets.get_unchecked(ix-1) + *counts.get_unchecked(ix-1) };
+            }
+            for ix in (0..ids_inner.len()) {
+                let id = unsafe{ *ids_inner.get_unchecked(ix) };
+                let byte = get_byte(id, offset);
+                let bucket = unsafe{ *buckets.get_unchecked(byte) };
+                unsafe{ *buffer.get_unchecked_mut(bucket) = id; }
+                unsafe{ *buckets.get_unchecked_mut(byte) = bucket + 1; }
+            }
         }
+        ::std::mem::swap(&mut buffer, ids);
     }
 }
 
@@ -65,113 +83,132 @@ mod tests {
     use rand::{Rng, SeedableRng, StdRng};
     use test::{Bencher, black_box};
 
-    fn nums(seed: usize) -> Vec<usize> {
+    fn ids(seed: usize) -> Vec<u32> {
         let n = 100_000;
         let mut rng = StdRng::new().unwrap();
         rng.reseed(&[seed+0, seed+1, seed+2, seed+3]);
-        let mut seen_nums = HashSet::new();
+        let mut seen_ids = HashSet::new();
         (0..n)
             .map(|_| rng.gen_range(0,n))
-            .filter(|num| seen_nums.insert(*num)) // filter out dupes
+            .filter(|id| seen_ids.insert(*id)) // filter out dupes
             .collect()
     }
 
     #[test]
     fn test_radix_sort() {
-        let mut nums_a = nums(7);
-        let mut nums_b = nums(7);
-        let mut nums_c = nums(7);
-        nums_a.sort();
-        let mut buffers = make_buffers(nums_b.len());
-        radix_sort(&mut nums_b, &mut buffers);
-        radix_sort(&mut nums_c, &mut buffers);
-        assert_eq!(nums_a, nums_b);
-        assert_eq!(nums_a, nums_c);
+        let mut ids_a = ids(7);
+        let mut ids_b = ids(7);
+        ids_a.sort();
+        radix_sort(&mut ids_b);
+        assert_eq!(ids_a, ids_b);
+    }
+
+    #[bench]
+    fn bench_sum(bencher: &mut Bencher) {
+        let ids = black_box(ids(7));
+        bencher.iter(|| {
+            let mut sum = 0;
+            for ix in (0..ids.len()) {
+                unsafe{ sum += *ids.get_unchecked(ix); }
+            }
+            black_box(sum);
+        });
+    }
+
+    #[bench]
+    fn bench_copy(bencher: &mut Bencher) {
+        let ids = black_box(ids(7));
+        let mut buffer = ids.clone();
+        bencher.iter(|| {
+            for ix in (0..ids.len()) {
+                unsafe{ *buffer.get_unchecked_mut(ix) = *ids.get_unchecked(ix); }
+            }
+            black_box(&buffer);
+        });
     }
 
     #[bench]
     fn bench_radix_sort(bencher: &mut Bencher) {
-        let nums = black_box(nums(7));
-        let mut buffers = make_buffers(nums.len());
+        let ids = black_box(ids(7));
         bencher.iter(|| {
-            let mut nums = nums.clone();
-            radix_sort(&mut nums, &mut buffers);
-            nums
+            let mut ids = ids.clone();
+            radix_sort(&mut ids);
+            black_box(&ids);
         });
     }
 
     #[bench]
     fn bench_hash_intersect(bencher: &mut Bencher) {
-        let nums_a = black_box(nums(7));
-        let nums_b = black_box(nums(42));
-        let max_size = max(nums_a.len(), nums_b.len());
+        let ids_a = black_box(ids(7));
+        let ids_b = black_box(ids(42));
+        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             let mut results = Vec::with_capacity(max_size);
             let mut index = HashSet::with_capacity(max_size);
-            for num in nums_a.iter() {
-                index.insert(*num);
+            for id in ids_a.iter() {
+                index.insert(*id);
             }
-            for num in nums_b.iter() {
-                if index.contains(num) {
-                    results.push(*num);
+            for id in ids_b.iter() {
+                if index.contains(id) {
+                    results.push(*id);
                 }
             }
-            return results
+            black_box(results);
         });
     }
 
     #[bench]
     fn bench_small_btree_intersect(bencher: &mut Bencher) {
-        let nums_a = black_box(nums(7));
-        let nums_b = black_box(nums(42));
-        let max_size = max(nums_a.len(), nums_b.len());
+        let ids_a = black_box(ids(7));
+        let ids_b = black_box(ids(42));
+        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             let mut results = Vec::with_capacity(max_size);
             let mut index = BTreeSet::with_b(10);
-            for num in nums_a.iter() {
-                index.insert(*num);
+            for id in ids_a.iter() {
+                index.insert(*id);
             }
-            for num in nums_b.iter() {
-                if index.contains(num) {
-                    results.push(*num);
+            for id in ids_b.iter() {
+                if index.contains(id) {
+                    results.push(*id);
                 }
             }
-            return results
+            black_box(results);
         });
     }
 
     #[bench]
     fn bench_large_btree_intersect(bencher: &mut Bencher) {
-        let nums_a = black_box(nums(7));
-        let nums_b = black_box(nums(42));
-        let max_size = max(nums_a.len(), nums_b.len());
+        let ids_a = black_box(ids(7));
+        let ids_b = black_box(ids(42));
+        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             let mut results = Vec::with_capacity(max_size);
             let mut index = BTreeSet::with_b(100);
-            for num in nums_a.iter() {
-                index.insert(*num);
+            for id in ids_a.iter() {
+                index.insert(*id);
             }
-            for num in nums_b.iter() {
-                if index.contains(num) {
-                    results.push(*num);
+            for id in ids_b.iter() {
+                if index.contains(id) {
+                    results.push(*id);
                 }
             }
-            return results
+            black_box(results);
         });
     }
 
     #[bench]
     fn bench_std_sort_intersect(bencher: &mut Bencher) {
-        let nums_a = black_box(nums(7));
-        let nums_b = black_box(nums(42));
-        let max_size = max(nums_a.len(), nums_b.len());
+        let ids_a = black_box(ids(7));
+        let ids_b = black_box(ids(42));
+        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             // the clone unfairly penalises this test, since in a real use we could just sort in place
-            let mut sorted_a = nums_a.clone();
-            let mut sorted_b = nums_b.clone();
+            let mut sorted_a = ids_a.clone();
+            let mut sorted_b = ids_b.clone();
             sorted_a.sort();
             sorted_b.sort();
-            let mut results: Vec<usize> = Vec::with_capacity(max_size);
+            let mut results: Vec<u32> = Vec::with_capacity(max_size);
             let mut ix_a = 0;
             let mut ix_b = 0;
             loop {
@@ -195,23 +232,22 @@ mod tests {
                     _ => break,
                 }
             }
-            return results
+            black_box(results);
         });
     }
 
     #[bench]
     fn bench_radix_sort_intersect(bencher: &mut Bencher) {
-        let nums_a = black_box(nums(7));
-        let nums_b = black_box(nums(42));
-        let max_size = max(nums_a.len(), nums_b.len());
-        let mut buffers = make_buffers(max_size);
+        let ids_a = black_box(ids(7));
+        let ids_b = black_box(ids(42));
+        let max_size = max(ids_a.len(), ids_b.len());
         bencher.iter(|| {
             // the clone unfairly penalises this test, since in a real use we could just sort in place
-            let mut sorted_a = nums_a.clone();
-            let mut sorted_b = nums_b.clone();
-            radix_sort(&mut sorted_a, &mut buffers);
-            radix_sort(&mut sorted_b, &mut buffers);
-            let mut results: Vec<usize> = Vec::with_capacity(max_size);
+            let mut sorted_a = ids_a.clone();
+            let mut sorted_b = ids_b.clone();
+            radix_sort(&mut sorted_a);
+            radix_sort(&mut sorted_b);
+            let mut results: Vec<u32> = Vec::with_capacity(max_size);
             let mut ix_a = 0;
             let mut ix_b = 0;
             loop {
@@ -235,7 +271,7 @@ mod tests {
                     _ => break,
                 }
             }
-            return results
+            black_box(results);
         });
     }
 }

34e76fb70a85ecbdec6a43d4dacdcb614a1609de
Jamie Brandon <jamie@scattered-thoughts.net>
2015-09-08 11:06:43 -0700

    stuff

--- src/join.rs
+++ src/join.rs
@@ -69,13 +69,14 @@ pub fn compare_by_key(left_words: &[u64], left_key: &[usize], right_words: &[u64
 }
 
 impl Chunk {
-    fn sort(&mut self, key: &[usize]) {
+    fn sort(&self, key: &[usize]) -> Chunk {
         let row_width = self.row_width;
+        let mut data = self.data.clone();
         let mut buffer = self.data.clone();
         for &word_ix in key.iter().rev() {
             let mut counts = [[0; 256]; 8];
-            for row_ix in (0..self.data.len()).step_by(row_width) {
-                let word = self.data[row_ix + word_ix];
+            for row_ix in (0..data.len()).step_by(row_width) {
+                let word = data[row_ix + word_ix];
                 let bytes = unsafe{ ::std::mem::transmute::<u64, [u8; 8]>(word) };
                 for byte_ix in (0..8) {
                     counts[byte_ix][bytes[byte_ix] as usize] += 1;
@@ -88,20 +89,20 @@ impl Chunk {
                 }
             }
             for byte_ix in (0..8) {
-                for row_ix in (0..self.data.len()).step_by(row_width) {
-                    let word = self.data[row_ix + word_ix];
+                for row_ix in (0..data.len()).step_by(row_width) {
+                    let word = data[row_ix + word_ix];
                     let bytes = unsafe{ ::std::mem::transmute::<u64, [u8; 8]>(word) };
                     let byte = bytes[byte_ix] as usize;
                     let bucket = buckets[byte_ix][byte];
                     for ix in (0..row_width) {
-                        buffer[bucket + ix] = self.data[row_ix + ix];
+                        buffer[bucket + ix] = data[row_ix + ix];
                     }
                     buckets[byte_ix][byte] += row_width;
                 }
-                ::std::mem::swap(&mut buffer, &mut self.data);
+                ::std::mem::swap(&mut buffer, &mut data);
             }
         }
-        self.sort_key = key.to_vec();
+        Chunk{data: data, row_width: self.row_width, sort_key: key.to_vec()}
     }
 
     fn groups<'a>(&'a self) -> Groups<'a> {
@@ -291,7 +292,7 @@ mod tests{
             id_pairs.into_iter().flat_map(|pair| pair).map(|&id| id).collect::<Vec<_>>()
         };
         let mut chunk = Chunk{data: ids.clone(), row_width: 2, sort_key: vec![]};
-        chunk.sort(&[0, 1]);
+        chunk = chunk.sort(&[0, 1]);
         assert_eq!(result_ids, chunk.data);
     }
 
@@ -312,8 +313,8 @@ mod tests{
         }
         let mut chunk_a = Chunk{ data: ids_a.clone(), row_width: 1, sort_key: vec![] };
         let mut chunk_b = Chunk{ data: ids_b.clone(), row_width: 1, sort_key: vec![] };
-        chunk_a.sort(key_a);
-        chunk_b.sort(key_b);
+        chunk_a = chunk_a.sort(key_a);
+        chunk_b = chunk_b.sort(key_b);
         let result_chunk = chunk_a.join(&chunk_b);
         assert_eq!(
             result_ids.chunks(2).collect::<HashSet<_>>(),
@@ -338,8 +339,8 @@ mod tests{
         }
         let mut chunk_a = Chunk{ data: ids_a.clone(), row_width: 2, sort_key: vec![] };
         let mut chunk_b = Chunk{ data: ids_b.clone(), row_width: 5, sort_key: vec![] };
-        chunk_a.sort(key_a);
-        chunk_b.sort(key_b);
+        chunk_a = chunk_a.sort(key_a);
+        chunk_b = chunk_b.sort(key_b);
         let result_chunk = chunk_a.join(&chunk_b);
         assert_eq!(
             result_ids.chunks(7).collect::<HashSet<_>>(),
@@ -352,7 +353,7 @@ mod tests{
         let ids = black_box(ids(7, 1_000_000));
         bencher.iter(|| {
             let mut chunk = Chunk{data: ids.clone(), row_width: 1, sort_key: vec![]};
-            chunk.sort(&[0]);
+            chunk = chunk.sort(&[0]);
             black_box(&chunk);
         });
     }
@@ -368,7 +369,7 @@ mod tests{
         }
         bencher.iter(|| {
             let mut chunk = Chunk{data: ids.clone(), row_width: 2, sort_key: vec![]};
-            chunk.sort(&[0, 1]);
+            chunk = chunk.sort(&[0, 1]);
             black_box(&chunk);
         });
     }
@@ -386,7 +387,7 @@ mod tests{
         }
         bencher.iter(|| {
             let mut chunk = Chunk{data: ids.clone(), row_width: 3, sort_key: vec![]};
-            chunk.sort(&[0, 1, 2]);
+            chunk = chunk.sort(&[0, 1, 2]);
             black_box(&chunk);
         });
     }
@@ -404,7 +405,7 @@ mod tests{
         }
         bencher.iter(|| {
             let mut chunk = Chunk{data: ids.clone(), row_width: 3, sort_key: vec![]};
-            chunk.sort(&[0]);
+            chunk = chunk.sort(&[0]);
             black_box(&chunk);
         });
     }
@@ -418,8 +419,8 @@ mod tests{
         bencher.iter(|| {
             let mut chunk_a = Chunk{ data: ids_a.clone(), row_width: 1, sort_key: vec![] };
             let mut chunk_b = Chunk{ data: ids_b.clone(), row_width: 1, sort_key: vec![] };
-            chunk_a.sort(key_a);
-            chunk_b.sort(key_b);
+            chunk_a = chunk_a.sort(key_a);
+            chunk_b = chunk_b.sort(key_b);
             let result_chunk = chunk_a.join(&chunk_b);
             black_box(result_chunk);
         })
@@ -434,8 +435,8 @@ mod tests{
         bencher.iter(|| {
             let mut chunk_a = Chunk{ data: ids_a.clone(), row_width: 3, sort_key: vec![] };
             let mut chunk_b = Chunk{ data: ids_b.clone(), row_width: 3, sort_key: vec![] };
-            chunk_a.sort(key_a);
-            chunk_b.sort(key_b);
+            chunk_a = chunk_a.sort(key_a);
+            chunk_b = chunk_b.sort(key_b);
             let result_chunk = chunk_a.join(&chunk_b);
             black_box(result_chunk);
         })
@@ -451,81 +452,3 @@ mod tests{
 //         };
 //         bytes / 8
 //     }
-
-//     fn comparable_width(&self) -> usize {
-//         let bytes = match *self {
-//             Kind::Id => size_of::<Id>(),
-//             Kind::Number => size_of::<Number>(),
-//             Kind::Text => size_of::<Hash>(), // don't compare the string pointer
-//         };
-//         bytes / 8
-//     }
-// }
-
-// impl Relation {
-//     fn len(&self) -> usize {
-//         self.data.len() / self.row_width
-//     }
-
-//     fn key(&self, key_fields: &[Id]) -> Vec<usize> {
-//         let column_widths = self.kinds.iter().map(|kind| kind.width()).collect::<Vec<_>>();
-//         let mut column_boundaries = vec![0; column_widths.len()];
-//         for column in (0..column_widths.len()-1) {
-//             column_boundaries[column+1] = column_boundaries[column] + column_widths[column];
-//         }
-//         let mut key = vec![];
-//         for &key_field in key_fields {
-//             let key_field_ix = self.fields.iter().position(|&field| field == key_field).unwrap();
-//             let start_ix = column_boundaries[key_field_ix];
-//             let end_ix = start_ix + self.kinds[key_field_ix].key_width();
-//             key.extend(start_ix..end_ix)
-//         }
-//         key
-//     }
-
-//     fn sort(&mut self, fields: &[Id]) {
-//         let key = self.key(fields);
-
-//         self.sort_key = key;
-//     }
-
-//     fn project(&self, other_fields: &[Id]) -> Relation {
-//         let other_field_ixes = other_fields.iter().map(|other_field|
-//             self.fields.iter().position(|self_field| self_field == other_field).unwrap()
-//             ).collect::<Vec<_>>();
-//         let other_kinds = other_field_ixes.iter().map(|&other_field_ix|
-//             self.kinds[other_field_ix]
-//             ).collect::<Vec<_>>();
-//         let other_row_width = other_kinds.iter().map(|kind| kind.width()).sum();
-//         let other_len = (self.data.len() / self.row_width) * other_row_width;
-//         let mut other_data = Vec::with_capacity(other_len);
-//         let other_key = self.key(other_fields);
-//         for row in self.data.chunks(self_row_width) {
-//             for &other_key_ix in other_key.iter() {
-//                 other_data.push(row[key_ix]);
-//             }
-//         }
-//         let other = Relation{
-//             fields: other_fields.to_vec(),
-//             kinds: other_kinds,
-//             data: other_data,
-//             sort_key: vec![],
-//         };
-//         other
-//         // TODO dedup other
-//     }
-
-//     fn total_join(&mut self, other: &mut Relation, self_key: &[usize], other_key: &[usize]) -> Relation {
-//         // a join where we know that every tuple contributes to the output
-//         assert_eq!(&*self.sort_key, self_key);
-//         assert_eq!(&*other.sort_key, other_key);
-//         assert_eq!(self_key.len(), other_key.len());
-//         let self_row_width = self.kinds.iter().map(|kind| kind.width()).sum();
-//         let other_row_width = other_kinds.iter().map(|kind| kind.width()).sum();
-//         let result_data = vec![];
-//         let self_len = self.data.len();
-//         let other_len = other.data.len();
-//         let mut self_ix = 0;
-//         let mut other_ix = 0;
-//     }
-// }

27c3604c401fd9e94f792ead7c2dd9a5cbb8bcc5
Jamie Brandon <jamie@scattered-thoughts.net>
2015-10-04 03:59:12 -0700

    Little repl

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@
 #![feature(iter_arith)]
 #![feature(step_by)]
 #![allow(dead_code)]
+#![feature(slice_patterns)]
 
 extern crate rand;
 extern crate test;
 extern crate time;
 extern crate regex;
-
-use regex::Regex;
 
 macro_rules! time {
     ($name:expr, $expr:expr) => {{
@@ -32,16 +31,24 @@ macro_rules! assert_set_eq {
 mod runtime;
 mod bootstrap;
 
+fn watch(filenames: &[String]) -> () {
+    loop {
+        print!("Loading...");
+        let bootstrap_program = bootstrap::Program::load(filenames);
+        print!("compiling...");
+        let mut runtime_program = bootstrap_program.compile();
+        print!("running...");
+        runtime_program.run();
+        println!("done!");
+        println!("{:#?}", runtime_program.states[runtime_program.states.len() - 1]);
+    }
+}
+
 fn main() {
-    let rex = Regex::new(r#"_|\?[:alnum:]*(:[:alnum:]*)?|"[^"]*"|([:digit:]|\.)+|#[:digit:]+"#).unwrap();
-    println!("{:?}", &[
-        rex.find("has ?cat bar"),
-        rex.find("has \"foo\" bar"),
-        rex.find("has 1 bar"),
-        rex.find("has 1.92 bar"),
-        rex.find("has # bar"),
-        rex.find("has #32 bar"),
-        rex.find("has no bar"),
-        rex.find("has a bar"),
-        ]);
+    use std::env;
+    let args = env::args().collect::<Vec<String>>();
+    match &*args[1] {
+        "--watch" => watch(&args[2..]),
+        _ => panic!("Didn't understand this command:\n {:?}", &args[1..]),
+    }
 }

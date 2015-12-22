7c9a13a1582e44f0e50f03a099bfc2b0cac30b87
Jamie Brandon <jamie@scattered-thoughts.net>
2015-12-22 00:28:44 +0000

    Tree

--- src/map.rs
+++ src/map.rs
@@ -17,6 +17,11 @@ struct Node {
     start: *mut u8,
 }
 
+struct Tree {
+    leaf_bytes: usize,
+    root: Node,
+}
+
 #[derive(Debug)]
 struct Layout {
     leaf_bytes: usize,
@@ -65,12 +70,13 @@ impl Node {
     }
 
     fn get_node_offset(&self, pos: usize) -> usize {
-        let ix = (self.get_header().node_bitmap << (32 - pos)).count_ones() as usize;
+        let ix = self.get_header().node_bitmap.wrapping_shl((32 - pos) as u32).count_ones() as usize;
         mem::size_of::<Header>() + (mem::size_of::<Node>() * ix)
     }
 
     fn get_leaf_offset(&self, pos: usize, layout: &mut Layout) -> usize {
-        let ix = (self.get_header().leaf_bitmap << (32 - pos)).count_ones() as usize;
+        let ix = self.get_header().leaf_bitmap.wrapping_shl((32 - pos) as u32).count_ones() as usize;
+        println!("{:?}", (self.get_header().leaf_bitmap, self.get_header().leaf_bitmap.wrapping_shl((32 - pos) as u32), ix, &layout));
         layout.allocated_bytes - ((ix + 1) * layout.leaf_bytes)
     }
 
@@ -79,11 +85,11 @@ impl Node {
             self.increase_size(layout);
         }
         unsafe {
-            (*(self.start as *mut Header)).node_bitmap |= 1 << pos;
             let node_offset = self.get_node_offset(pos);
             let later_node_bytes = mem::size_of::<Header>() + layout.total_node_bytes - node_offset;
             ptr::copy(self.start.offset(node_offset as isize), self.start.offset((node_offset + 1) as isize), later_node_bytes);
             *(self.start.offset(node_offset as isize) as *mut Node) = node;
+            (*(self.start as *mut Header)).node_bitmap |= 1 << pos;
         }
     }
 
@@ -93,7 +99,6 @@ impl Node {
         }
         unsafe {
             println!("layout claims {:?}", &layout);
-            (*(self.start as *mut Header)).leaf_bitmap |= 1 << pos;
             let leaf_offset = self.get_leaf_offset(pos, layout);
             let leaves_offset = layout.allocated_bytes - layout.total_leaf_bytes;
             println!("copying {:?} {:?} {:?}", leaves_offset, leaves_offset - layout.leaf_bytes, leaf_offset + layout.leaf_bytes - leaves_offset);
@@ -104,6 +109,7 @@ impl Node {
                 );
             println!("copying {:?} {:?} {:?}", "leaf", leaf_offset, layout.leaf_bytes);
             ptr::copy(leaf.as_ptr() as *const u8, self.start.offset(leaf_offset as isize), layout.leaf_bytes);
+            (*(self.start as *mut Header)).leaf_bitmap |= 1 << pos;
         }
     }
 
@@ -130,6 +136,63 @@ impl Node {
         let layout = self.layout(leaf_bytes);
         unsafe {
             slice::from_raw_parts_mut(self.start as *mut u64, layout.allocated_bytes / 8)
+        }
+    }
+}
+
+impl Tree {
+    fn new(leaf_bytes: usize) -> Self {
+        Tree{
+            leaf_bytes: leaf_bytes,
+            root: Node::new(),
+        }
+    }
+
+    fn insert (&mut self, leaf: &[u64]) {
+        assert_eq!(self.leaf_bytes, leaf.len() * 8);
+        let mut node = &mut self.root;
+        for word_ix in 0..leaf.len() {
+            let word = leaf[word_ix];
+            for ix in 0..13 {
+                let pos = (word >> (ix*5)) & 0b11111;
+                let header = node.get_header();
+                let mut layout = node.layout(self.leaf_bytes);
+                if (header.node_bitmap & (1 << (pos as u32))) > 0 {
+                    let child_offset = node.get_node_offset(pos as usize);
+                    let child = unsafe {
+                        &mut *(node.start.offset(child_offset as isize) as *mut Node)
+                    };
+                    node = child;
+                } else if (header.leaf_bitmap & (1 << (pos as u32))) > 0 {
+                    let mut layout = node.layout(self.leaf_bytes);
+                    let blocking_leaf_offset = node.get_leaf_offset(pos as usize, &mut layout);
+                    let blocking_leaf = unsafe {
+                        slice::from_raw_parts(node.start.offset(blocking_leaf_offset as isize) as *mut u64, self.leaf_bytes / 8)
+                    };
+                    if leaf == blocking_leaf {
+                        return;
+                    } else {
+                        // TODO delete leaf
+                        let mut child = Node::new();
+                        let (next_word_ix, next_ix) = if ix == 12 {
+                            assert!(word_ix + 1 < leaf.len());
+                            (word_ix + 1, 0)
+                        } else {
+                            (word_ix, ix + 1)
+                        };
+                        let next_pos = (leaf[next_word_ix] >> (next_ix * 5)) & 0b11111;
+                        let mut layout = child.layout(self.leaf_bytes);
+                        child.insert_leaf(&mut layout, leaf, next_pos as usize);
+                        let next_blocking_pos = (blocking_leaf[next_word_ix] >> (next_ix * 5)) & 0b11111;
+                        let mut layout = child.layout(self.leaf_bytes);
+                        child.insert_leaf(&mut layout, blocking_leaf, next_blocking_pos as usize);
+                        node.insert_node(&mut layout, child, pos as usize);
+                    }
+                } else {
+                    node.insert_leaf(&mut layout, leaf, pos as usize);
+                    return;
+                }
+            }
         }
     }
 }
@@ -162,4 +225,16 @@ pub fn main() {
     println!("{:?}", node.layout(leaf_bytes));
     println!("{:?}", node.as_slice(leaf_bytes));
     println!("after");
+
+    let mut tree = Tree::new(leaf_bytes);
+    tree.insert(&[1, 2, 3]);
+    println!("insert {:?}", &[1, 2, 3]);
+    tree.insert(&[7, 8, 9]);
+    println!("insert {:?}", &[7, 8, 9]);
+    tree.insert(&[4, 5, 6]);
+    println!("insert {:?}", &[4, 5, 6]);
+    tree.insert(&[1, 2, 3]);
+    println!("insert {:?}", &[1, 2, 3]);
+    tree.insert(&[1, 1, 1]);
+    println!("insert {:?}", &[1, 1, 1]);
 }

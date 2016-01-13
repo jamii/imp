b4ecf1859157aa427742df2ae70615a46be3a779
Jamie Brandon <jamie@scattered-thoughts.net>
2016-01-13 09:44:44 +0000

    Working Hamt

--- src/Hamt.jl
+++ src/Hamt.jl
@@ -11,213 +11,213 @@ macro node(n)
 end)
 end
 
-# aims to fit into pool sizes at https://github.com/JuliaLang/julia/blob/6cc48dcd24322976bdc193b3c578acb924f0b8e9/src/gc.c#L1308-L1336
-@node(0)
-@node(3)
-@node(7)
-@node(15)
-@node(31)
+# aims to fit into pool sizes - see https://github.com/JuliaLang/julia/blob/6cc48dcd24322976bdc193b3c578acb924f0b8e9/src/gc.c#L1308-L1336
+@node(2)
+@node(4)
+@node(8)
+@node(16)
 @node(32)
 
 # TODO assert that offsets are correct, fields are in order etc
 const bitmap_offset = 0
 const nodes_offset = sizeof(Ptr)
 
-const not_found = "not_found" # arbitrary pointer to compare to
+Base.call(::Type{Node2}) = Node2(0, unsafe_pointer_to_objref(C_NULL), unsafe_pointer_to_objref(C_NULL))
 
-grow(node_before::Ptr{Node}, size_before::Integer, size_after::Integer, type_after::Type) = begin
-  pointer_before = convert(Ptr{UInt8}, node_before)
-  pointer_after = ccall((:jl_gc_allocobj, :libjulia), Ptr{UInt8}, (Csize_t,), size_after)
-  unsafe_store!(convert(Ptr{Ptr{Void}}, pointer_after), pointer_from_objref(type_after), 0)
+grow(node_pointer::Ptr{Node}, size_before::Integer, size_after::Integer, type_after::Type) = begin
+  node_before = unsafe_load(convert(Ptr{Ptr{UInt8}}, node_pointer))
+  node_after = ccall((:jl_gc_allocobj, :libjulia), Ptr{UInt8}, (Csize_t,), size_after)
+  unsafe_store!(convert(Ptr{Ptr{Void}}, node_after), pointer_from_objref(type_after), 0)
   for i in 1:size_before
-    unsafe_store!(pointer_after, unsafe_load(pointer_before, i), i)
+    unsafe_store!(node_after, unsafe_load(node_before, i), i)
   end
   for i in (size_before+1):size_after
-    unsafe_store!(pointer_after, 0, i)
+    unsafe_store!(node_after, 0, i)
   end
-  convert(Ptr{Node}, pointer_after)
+  unsafe_store!(convert(Ptr{Ptr{UInt8}}, node_pointer), node_after)
+  nothing
 end
 
 # TODO figure out how to get a computed goto out of this
-maybe_grow(node_before::Ptr{Node}, length::Integer) = begin
-  if length == 0
-    grow(node_before, sizeof(Node0), sizeof(Node3), Node3)
-  elseif length == 3
-    grow(node_before, sizeof(Node3), sizeof(Node7), Node7)
-  elseif length == 7
-    grow(node_before, sizeof(Node7), sizeof(Node15), Node7)
-  elseif length == 15
-    grow(node_before, sizeof(Node15), sizeof(Node31), Node7)
-  elseif length == 31
-    grow(node_before, sizeof(Node31), sizeof(Node32), Node7)
-  else
-    node_before
+maybe_grow(node_pointer::Ptr{Node}, length::Integer) = begin
+  if length == 2
+    grow(node_pointer, sizeof(Node2), sizeof(Node4), Node4)
+  elseif length == 4
+    grow(node_pointer, sizeof(Node4), sizeof(Node8), Node8)
+  elseif length == 8
+    grow(node_pointer, sizeof(Node8), sizeof(Node16), Node16)
+  elseif length == 16
+    grow(node_pointer, sizeof(Node16), sizeof(Node32), Node32)
   end
+  nothing
 end
 
-get_bitmap(node::Ptr{Node}) = begin
+@inline get_bitmap(node::Ptr{Void}) = begin
   unsafe_load(convert(Ptr{UInt32}, node + bitmap_offset))
 end
 
-set_bitmap!(node::Ptr{Node}, bitmap::Integer) = begin
+@inline set_bitmap!(node::Ptr{Void}, bitmap::Integer) = begin
   unsafe_store!(convert(Ptr{UInt32}, node + bitmap_offset), UInt32(bitmap))
 end
 
-get_node(node::Ptr{Node}, pos::Integer) = begin
-  unsafe_load(convert(Ptr{Any}, node + nodes_offset), pos)
+@inline get_node(node::Ptr{Void}, pos::Integer) = begin
+  convert(Ptr{Node}, node + nodes_offset + ((pos-1) * sizeof(Ptr)))
 end
 
-set_node!(node::Ptr{Node}, val, pos::Integer) = begin
-  unsafe_store!(convert(Ptr{Any}, node + nodes_offset), val, pos)
+@inline set_node!(node::Ptr{Void}, val::Ptr{Void}, pos::Integer) = begin
+  unsafe_store!(convert(Ptr{Ptr{Void}}, get_node(node, pos)), val)
 end
 
-get_pos(bitmap::UInt32, ix::Integer) = begin
+@inline get_pos(bitmap::UInt32, ix::Integer) = begin
   1 + count_ones(bitmap << (32 - ix))
 end
 
-get_child_inner(node::Ptr{Node}, ix::Integer) = begin
+get_child(node_pointer::Ptr{Node}, ix::Integer) = begin
+  node = unsafe_load(convert(Ptr{Ptr{Void}}, node_pointer))
   bitmap = get_bitmap(node)
   if (bitmap & (1 << ix)) == 0
-    not_found
+    convert(Ptr{Node}, 0)
   else
     get_node(node, get_pos(bitmap, ix))
   end
 end
 
-set_child_inner!(node::Ptr{Node}, val::ANY, ix::Integer) = begin
-  val_pointer = pointer_from_objref(val)
+set_child!(node_pointer::Ptr{Node}, val::ANY, ix::Integer) = begin
+  node = unsafe_load(convert(Ptr{Ptr{Void}}, node_pointer))
   bitmap = get_bitmap(node)
   pos = get_pos(bitmap, ix)
   if (bitmap & (1 << ix)) == 0
     length = count_ones(bitmap)
-    node = maybe_grow(node, length)
+    maybe_grow(node_pointer, length)
+    node = unsafe_load(convert(Ptr{Ptr{Void}}, node_pointer))
     set_bitmap!(node, bitmap | (1 << ix))
     for i in length:-1:pos
-      set_node!(node, get_node(node, i), i+1)
+      set_node!(node, unsafe_load(convert(Ptr{Ptr{Void}}, get_node(node, i))), i+1)
     end
   end
-  set_node!(node, val, pos)
+  set_node!(node, pointer_from_objref(val), pos)
   # TODO figure out how to link this
   # ccall((:jl_gc_wb, :libjulia), Void, (Ptr{Void}, Ptr{Void}), node, val_pointer)
   ccall((:jl_gc_queue_root, :libjulia), Void, (Ptr{Void},), node)
-  node
+  nothing
 end
-
-@inline get_child(node::Node, ix::Integer) = begin
-  get_child_inner(convert(Ptr{Node}, pointer_from_objref(node)), ix)
-end
-
-@inline set_child!(node::Node, val::ANY, ix::Integer) = begin
-  unsafe_pointer_to_objref(set_child_inner!(convert(Ptr{Node}, pointer_from_objref(node)), val, ix))::Node
-end
-
-### NO UNSAFE BELOW THIS LINE ###
-
-n = Node0(0)
-n = set_child!(n, 3, 3)
-n = set_child!(n, 5, 5)
-n = set_child!(n, 1, 1)
-n = set_child!(n, 0, 0)
-n = set_child!(n, 21, 21)
-n = set_child!(n, 99, 31)
-n = set_child!(n, 31, 31)
 
 type Tree{T}
   root::Node
 end
 
-call{T}(::Type{Tree{T}}) = Tree{T}(Node0(0))
+const root_offset = 0
+
+call{T}(::Type{Tree{T}}) = Tree{T}(Node2())
 
 const key_length = Int64(ceil(sizeof(hash(0)) * 8.0 / 5.0))
 
 chunk_at(key, ix) = (key >> (ix*5)) & 0b11111
 
-singleton(row, column, ix) = begin
+@noinline out_of_bits() = error("Out of bits!")
+
+reinsert{T}(node_pointer::Ptr{Node}, row::T, column::Integer, ix::Integer) = begin
   if ix >= key_length
     column += 1
     ix = 0
     if column > length(row)
-      error("Out of bits")
+      out_of_bits()
     end
   end
   value = row[column]
   key = value # key = hash(value)
   chunk = chunk_at(key, ix)
-  node = Node0(0)
-  set_child!(node, row, ix)
+  set_child!(node_pointer, row, chunk)
 end
 
 Base.push!{T}(tree::Tree{T}, row::T) = begin
-  node::Node = tree.root
+  node_pointer = convert(Ptr{Node}, pointer_from_objref(tree) + root_offset)
   for column in 1:length(row)
     value = row[column]
     key = value # TODO key = hash(value)
     for ix in 0:(key_length-1)
       chunk = chunk_at(key, ix)
-      child = get_child(node, chunk)
-      if child === not_found
+      child_pointer = get_child(node_pointer, chunk)
+      if convert(UInt, child_pointer) === UInt(0)
         # empty slot
-        set_child!(node, row, chunk)
-        return tree
-      elseif typeof(child) !== t
-        node = child
-        # continue loop
-      elseif child == row
-        # dupe
+        set_child!(node_pointer, row, chunk)
         return tree
       else
-        # collision
-        new_node = singleton(leaf, column, ix+1)
-        set_child!(node, new_node, chunk) # TODO this calls popcnt again
-        node = new_node
-        # continue loop
+        child = unsafe_pointer_to_objref(unsafe_load(convert(Ptr{Ptr{Any}}, child_pointer)))
+        if typeof(child) !== T
+          node_pointer = child_pointer
+          # continue loop
+        elseif child::T == row # TODO extra typeassert is annoying
+          # dupe
+          return tree
+        else
+          # collision
+          # TODO this calls popcnt more than necessary
+          set_child!(node_pointer, Node2(), chunk)
+          node_pointer = get_child(node_pointer, chunk)
+          reinsert(node_pointer, child::T, column, ix+1) # TODO extra typeassert is annoying
+          # continue loop
+        end
       end
     end
   end
-  error("Out of bits!")
+  out_of_bits()
 end
 
 Base.in{T}(row::T, tree::Tree{T}) = begin
-  node = tree.root
+  node_pointer = convert(Ptr{Node}, pointer_from_objref(tree) + root_offset)
   for column in 1:length(row)
     value = row[column]
     key = value # TODO key = hash(value)
     for ix in 0:(key_length-1)
       chunk = chunk_at(key, ix)
-      child = get_child(node, chunk)
-      if child === not_found
+      child_pointer = get_child(node_pointer, chunk)
+      if convert(UInt, child_pointer) == 0
         return false
-      elseif typeof(child) !== t
-        node = child
-        # continue loop
       else
-        return child == row
+        child = unsafe_pointer_to_objref(unsafe_load(convert(Ptr{Ptr{Any}}, child_pointer)))
+        if typeof(child) !== T
+          node_pointer = child_pointer
+          # continue loop
+        else
+          return child::T == row # TODO extra typeassert is annoying
+        end
       end
     end
   end
-  error("Out of bits!")
+  out_of_bits()
 end
 
 ids() = ids(1000000)
 ids(n) = rand(UInt64, n)
 
+type A
+  a::UInt
+end
+
+Base.length(::A) = 1
+Base.getindex(a::A, _) = a.a
+
 f(ids) = begin
-  tree = Tree{Tuple{UInt64}}()
-  push!(tree, (UInt64(1),))
-  (UInt64(1),) in tree
-  rows = Any[(a,) for a in ids] # forces boxing
+  # tree = Tree{A}()
+  # push!(tree, (UInt64(1),))
+  rows = [A(a) for a in ids]
   gc_enable(false)
   for _ in 1:10
-    tree = Tree{Tuple{UInt64}}()
+    tree = Tree{A}()
     @time begin
       for row in rows
         push!(tree, row)
       end
     end
     @time begin
-      println(all([(row in tree) for row in rows]))
+      for row in rows
+        if !(row in tree)
+          error("missing")
+        end
+      end
     end
-    tree = Tree{Tuple{UInt64}}()
+    tree = Tree{A}()
     @time begin
       gc_enable(true)
       gc()
@@ -227,6 +227,10 @@ f(ids) = begin
   end
 end
 
-f(ids(1))
+srand(999)
+f(ids(10000000))
+# Profile.clear_malloc_data()
+# srand(999)
+# f(ids(1000000))
 
 end

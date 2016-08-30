5e791030810780a8076504e78585ddb7c77316e6
Jamie Brandon <jamie@scattered-thoughts.net>
2016-08-30 19:35:04 +0200

    Cleaner sketch

--- diary.md
+++ diary.md
@@ -7017,3 +7017,98 @@ Graph.bench()
 Chinook.bench()
 Job.bench()
 ```
+
+I need some examples to work with to figure out what to implement next. I started with a simple minesweeper game. I don't think it's a particularly good usecase for Imp, but someone recently posted an Eve version and I was feeling cheeky. A sketch of the core mechanics:
+
+``` julia
+function run(num_x, num_y, num_mines)
+  @relation state() => Symbol
+  @relation mine(Int64, Int64)
+  @relation mine_count(Int64, Int64) => Int64
+  @relation cleared(Int64, Int64)
+  @relation clicked(Int64) => Int64, Int64
+  
+  @query begin 
+    + state() = :game_ok
+  end
+  
+  while length(mine) < num_mines
+    @query begin 
+      x = rand(1:num_x)
+      y = rand(1:num_y)
+      + mine(x,y)
+    end 
+  end
+  
+  @query begin 
+    x in 1:num_x
+    y in 1:num_y
+    c = count(
+      nx in -1:1
+      ny in -1:1
+      @when (nx != 0) || (ny != 0)
+      mine(x+nx, y+ny) = true
+    )
+    + mine_count(x, y) = c
+  end
+  
+  @Window(clicked) do display, event_number
+    @query begin
+      clicked($event_number) = (x, y)
+      + cleared(x, y)
+    end
+    
+    fix!(cleared) do
+      @query begin 
+        cleared(x,y)
+        mine_count(x,y,0)
+        nx in -1:1
+        ny in -1:1
+        @when (nx * ny == 0) && (nx + ny != 0) # no boolean xor :(
+        + cleared(x+nx, y+ny)
+      end)
+    end
+    
+    @query 
+      clicked($event_number) = (x, y)
+      mine(x,y)
+      + state() = :game_over
+    end
+    
+    @query begin 
+      state() = state
+      x in 1:num_x
+      y in 1:num_y
+      cleared = exists(cleared(x,y))
+      mine = exists(mine(x,y))
+      mine_count(x,y,count)
+      node = @match (state, mine, cleared, count) begin
+        (:game_over, true, _, _) => button("💣")
+        (:game_over, false, _, _) => button(string(count))
+        (:game_ok, _, true, 0) => button(" ")
+        (:game_ok, _, true, _) => button(string(count))
+        (:game_ok, _, false, _) => button("X", :onclick => @event clicked(x,y))
+      end
+      @group y node = h_box(node)
+      @group x node = v_box(node)
+      + display() = node
+    end
+    
+  end
+end
+```
+
+This requires:
+
+* a relation macro that records a functional dependecy 
+* query syntax updated to match 
+* syntax for upsert into a relation 
+* (probably also want delete)
+* with change tracking to handle fix!
+* better aggregates / subqueries / negation 
+
+The last point is a design problem that has been bugging me for ages, so it bears some thinking about. 
+
+Fundeps / upsert is simpler, but it does move Imp away from being a general purpose library. It probably won't be hard to support a separate macro that just returns results though.
+
+I was imagining that eg `+ mine_count(x, y) = c` would replace any existing value for `(x, y)`, but what should happen if a single query execution produces multiple values of `c` for a single `(x,y)`. Probably an error?
--- examples/Minesweeper.jl
+++ examples/Minesweeper.jl
@@ -5,127 +5,78 @@ using Query
 using UI
 using Match
 
-function fix!(flow, relation)
-  # TODO semi-naive
-  while true 
-    if union!(relation, flow())
-      break;
-    end
-  end
-end
-
 function run(num_x, num_y, num_mines)
-  state = @query([s::symbol],
-  begin
-    s = :game_ok
-  end)
+  @relation state() => Symbol
+  @relation mine(Int64, Int64)
+  @relation mine_count(Int64, Int64) => Int64
+  @relation cleared(Int64, Int64)
+  @relation clicked(Int64) => Int64, Int64
   
-  mine = @query([x::Int64, y::Int64, m::Bool],
-  begin 
+  @query begin 
+    + state() = :game_ok
+  end
+  
+  while length(mine) < num_mines
+    @query begin 
+      x = rand(1:num_x)
+      y = rand(1:num_y)
+      + mine(x,y)
+    end 
+  end
+  
+  @query begin 
     x in 1:num_x
     y in 1:num_y
-    m = false
-  end)
+    c = count(
+      nx in -1:1
+      ny in -1:1
+      @when (nx != 0) || (ny != 0)
+      mine(x+nx, y+ny) = true
+    )
+    + mine_count(x, y) = c
+  end
   
-  union!(mine, @query([x::Int64, y::Int64, m::Bool],
-  begin 
-    i in 1:num_mines
-    x = rand(1:num_x)
-    y = rand(1:num_y)
-    m = true
-  end)) # TODO check for dupes
-  
-  mine_count = @query([x::Int64, y::Int64, c::Int64],
-  begin 
-    x in 1:num_x
-    y in 1:num_y
-    c = 0
-  end),
-
-  union!(mine_count, @query([x::Int64, y::Int64],
-  begin 
-    x in 1:num_x
-    y in 1:num_y
-    nx in -1:1
-    ny in -1:1
-    @when (nx != 0) || (ny != 0)
-    mine(x+nx, y+ny, true)
-  end))
-  
-  click = Relation(Int64[], Int64[], Int64[])
-  cleared = @query([x::Int64, y::Int64, c::Bool],
-  begin
-    x in 1:num_x
-    y in 1:num_y
-    c = false
-  end)
-  
-  @Window(click) do window, event_number
-    
-    union!(cleared, @query([x::Int64, y::Int64],
-    begin
-      event_number = event_number # TODO need a nicer way to ref external variables
-      click(event_number, x, y)
-    end))
+  @Window(clicked) do display, event_number
+    @query begin
+      clicked($event_number) = (x, y)
+      + cleared(x, y)
+    end
     
     fix!(cleared) do
-      @query([new_x::Int64, new_y::Int64, c::Bool],
-      begin 
-        cleared(x,y,true)
+      @query begin 
+        cleared(x,y)
         mine_count(x,y,0)
         nx in -1:1
         ny in -1:1
         @when (nx * ny == 0) && (nx + ny != 0) # no boolean xor :(
-        new_x = x + nx
-        new_y = y + ny
-        c = true
+        + cleared(x+nx, y+ny)
       end)
     end
     
-    union!(state, @query([s::Symbol],
-    begin
-      event_number = event_number # TODO need a nicer way to ref external variables
-      clicked(event_number, x, y)
-      mine(x,y,true)
-      s = :game_over
-    end))
+    @query 
+      clicked($event_number) = (x, y)
+      mine(x,y)
+      + state() = :game_over
+    end
     
-    display = @query([x::Int64, y::Int64, node],
-    begin 
-      state(state)
+    @query begin 
+      state() = state
       x in 1:num_x
       y in 1:num_y
-      cleared(x,y,cleared)
-      mine(x,y,mine)
+      cleared = exists(cleared(x,y))
+      mine = exists(mine(x,y))
       mine_count(x,y,count)
       node = @match (state, mine, cleared, count) begin
-        (:game_over, true, _, _) => button("", "💣")
-        (:game_over, false, _, _) => button("", string(count))
-        (:game_ok, _, true, 0) => button("", " ")
-        (:game_ok, _, true, _) => button("", string(count))
-        (:game_ok, _, false, _) => button("", Dict(:onclick => @event click(x,y)), "X")
+        (:game_over, true, _, _) => button("💣")
+        (:game_over, false, _, _) => button(string(count))
+        (:game_ok, _, true, 0) => button(" ")
+        (:game_ok, _, true, _) => button(string(count))
+        (:game_ok, _, false, _) => button("X", :onclick => @event clicked(x,y))
       end
-    end)
+      @group y node = h_box(node)
+      @group x node = v_box(node)
+      + display() = node
+    end
     
-    # TODO need better aggregates for display
-    # TODO need some form of ordering
-    
-    display = @query([y::Int64],
-    ([], push_exp!, node),
-    begin
-      display(x,y,node)
-    end)
-    
-    display = @query([],
-    ([], push_exp!, node),
-    begin 
-      display(y, nodes)
-      node = h_box(nodes)
-    end)
-    
-    node = v_box(display.columns[1][1])
-    
-    body!(window, node)
-      
   end
 end

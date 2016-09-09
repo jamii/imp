90f4a962e3bc8435671706adf1b921cdc5e745c7
Jamie Brandon <jamie@scattered-thoughts.net>
2016-09-09 18:26:57 +0200

    Win condition

--- examples/Minesweeper.jl
+++ examples/Minesweeper.jl
@@ -38,22 +38,6 @@ function vbox(nodes)
   Hiccup.div(nodes)
 end
 
-macroexpand(quote
-@merge! mine_count begin 
-  x in 1:num_x
-  y in 1:num_y
-  neighbouring_mines = @query begin
-    nx in (x-1):(x+1)
-    ny in (y-1):(y+1)
-    @when (nx != x) || (ny != y)
-    mine(nx, ny) 
-    return (nx::Int64, ny::Int64)
-  end
-  c = length(neighbouring_mines)
-  return (x::Int64, y::Int64, c::Int64)
-end
-end)
-
 function run(num_x, num_y, num_mines)
   srand(999)
   
@@ -65,7 +49,7 @@ function run(num_x, num_y, num_mines)
   @relation display() = Hiccup.Node
   
   @merge! state begin 
-    s = :game_ok
+    s = :game_in_progress
     return (s::Symbol,)
   end
   
@@ -104,16 +88,28 @@ function run(num_x, num_y, num_mines)
     @fix! cleared begin 
       cleared(x,y)
       mine_count(x,y,0)
-      pos in [(x-1,y), (x+1, y), (x, y-1), (x, y+1)]
-      nx = pos[1]
-      ny = pos[2]
+      nx in (x-1):(x+1)
+      ny in (y-1):(y+1)
+      @when nx in 1:num_x
+      @when ny in 1:num_y
+      @when (nx != x) || (ny != y)
       return (nx::Int64, ny::Int64)
+    end
+    
+    @merge! state begin 
+      num_cleared = length(@query begin
+        cleared(x,y)
+        return (x::Int64, y::Int64)
+      end)
+      @when num_cleared + num_mines >= num_x * num_y
+      s = :game_won
+      return (s::Symbol,)
     end
     
     @merge! state begin
       clicked($event_number, x, y)
       mine(x,y)
-      s = :game_over
+      s = :game_lost
       return (s::Symbol,)
     end
     
@@ -135,11 +131,11 @@ function run(num_x, num_y, num_mines)
           return (count::Int64,)
         end).columns[1][1]
         return @match (current_state, is_mine, is_cleared, count) begin
-         (:game_over, true, _, _) => button("💣")
-         (:game_over, false, _, _) => button(string(count))
-         (:game_ok, _, true, 0) => button("_")
-         (:game_ok, _, true, _) => button(string(count))
-         (:game_ok, _, false, _) => button(Dict(:onclick => @event clicked(x,y)), "X")
+         (:game_in_progress, _, true, 0) => button("_")
+         (:game_in_progress, _, true, _) => button(string(count))
+         (:game_in_progress, _, false, _) => button(Dict(:onclick => @event clicked(x,y)), "X")
+         (_, true, _, _) => button("💣")
+         (_, false, _, _) => button(string(count))
          _ => error()
        end
      end)
@@ -152,6 +148,6 @@ function run(num_x, num_y, num_mines)
   (state, mine, mine_count, clicked, display, cleared)
 end
 
-(state, mine, mine_count, clicked, display, cleared) = run(10, 20, 1)
+(state, mine, mine_count, clicked, display, cleared) = run(10, 20, 10)
 
 end

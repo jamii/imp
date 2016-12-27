2019e52fd83cf4d26ea10c9de6bddbddc85dde65
Jamie Brandon <jamie@scattered-thoughts.net>
2016-12-27 15:24:50 +0100

    Separate out class and text changes from tree changes

--- src/UI.jl
+++ src/UI.jl
@@ -34,33 +34,51 @@ pre = @transient node(Id) => (Id, Int64, String, String, String)
 
 post = Sequence([
   # (level, parent, ix, id, kind, class, text)
-  @transient sorted_node(Int64, Id, Int64, Id, String, String, String)
+  @transient sorted_node(Int64, Id, Int64, Id, String)
 
   @merge begin
     root = UI.root
-    node(id) => (root, ix, kind, class, text)
-    return sorted_node(1, root, ix, id, kind, class, text)
+    node(id) => (root, ix, kind, _, _)
+    return sorted_node(1, root, ix, id, kind)
   end
   
   Fixpoint(
     @merge begin
-      sorted_node(level, _, _, parent, _, _, _)
-      node(id) => (parent, ix, kind, class, text)
-      return sorted_node(level+1, parent, ix, id, kind, class, text)
+      sorted_node(level, _, _, parent, _,)
+      node(id) => (parent, ix, kind, _, _)
+      return sorted_node(level+1, parent, ix, id, kind)
     end
   )
+  
+  @transient class(Id, String)
+  
+  @merge begin
+    node(id) => (_, _, _, class, _)
+    return class(id, class)
+  end
+  
+  @transient text(Id, String)
+  
+  @merge begin
+    node(id) => (_, _, _, _, text)
+    return text(id, text)
+  end
 ])
 
 function render(window, old_state, new_state)
   (removed, inserted) = Data.diff(old_state[:sorted_node], new_state[:sorted_node])
-  (_, _, _, removed_id, _, _, _) = removed
-  (_, parent, ix, id, kind, class, text) = inserted
-  @js(window, render($removed_id, $parent, $ix, $id, $kind, $class, $text))
+  (_, _, _, removed_id, _) = removed
+  (_, parent, ix, id, kind) = inserted
+  (_, (class_id, class)) = Data.diff(old_state[:class], new_state[:class])
+  (_, (text_id, text)) = Data.diff(old_state[:text], new_state[:text])
+  @js(window, render($removed_id, $parent, $ix, $id, $kind, $class_id, $class, $text_id, $text))
 end
 
 function render(window, state)
-  (_, id, parent, ix, kind, class, text) = state[:sorted_node].columns
-  @js(window, render($([]), $id, $parent, $ix, $kind, $class, $text))
+  (_, parent, ix, id, kind) = state[:sorted_node].columns
+  (class_id, class) = state[:class].columns
+  (text_id, text) = state[:text].columns
+  @js(window, render($([]), $parent, $ix, $id, $kind, $class_id, $class, $text_id, $text))
 end
 
 function window(world)

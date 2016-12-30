b769b77a13a76bbaeca4f93431fd21838ad00c19
Jamie Brandon <jamie@scattered-thoughts.net>
2016-12-30 12:28:49 +0100

    Minesweeper!

--- src/LB.jl
+++ src/LB.jl
@@ -7,9 +7,9 @@ function lb(command)
   write("/tmp/script.lb", command)
   output = readstring(`/home/jamie/logicblox-x86_64-linux-4.3.17-a3f4888aecd21c326197b02deae87d1d74569cc5/bin/lb /tmp/script.lb`)
   raw_results = matchall(r"/--------------- _ ---------------\\([^$\\]*)\\--------------- _ ---------------/"s, output)
-  stripped_results = [raw_result[37:(length(raw_result)-36)] for raw_result in raw_results]
+  stripped_results = [string("[", raw_result[37:(length(raw_result)-36)], "]") for raw_result in raw_results]
   @show stripped_results
-  results = Any[readtable(IOBuffer(String(stripped_result)), separator=' ', header=false) for stripped_result in stripped_results]
+  results = Any[eval(parse(stripped_result)) for stripped_result in stripped_results]
   @show results
 end
 
@@ -17,7 +17,7 @@ begin
   init = """
   open imp
   
-  addblock '
+  addblock --name ui '
     node(id; parent, ix, tag) ->
       string(id), string(parent), int(ix), string(tag).
       
@@ -55,42 +55,8 @@ begin
   lb(init)
 end
 
-begin 
-  command = """
-    open imp
-    
-    exec '
-      -node(id; _, _, _) <-
-        node@prev(id; _, _, _).
-    '
-  """
-  lb(command)
-end
-
-begin 
-  command = """
-    open imp
-    
-    exec '
-      +node("a"; "root", 1, "div").
-      +node("b"; "root", 2, "div").
-      +node("c"; "a", 1, "button").
-      
-      +text("c"; "click me!").
-      
-      +onclick("c").
-    '
-    
-    query '_(l, id) <-
-      node(id; _, _, _),
-      level(id; l).
-    '    
-  """
-  lb(command)
-end
-
-function col(dataframe, i)
-  (size(dataframe) == (0,0)) ? Any[] : dataframe[i].data
+function col(matrix, i)
+  (matrix == nothing || matrix == []) ? Any[] : matrix[:,i]
 end
   
 
@@ -131,7 +97,7 @@ function render(window, event_name, event_values)
     '
   """
   (removed, inserted, style, text, onclick, onkeydown) = lb(command)
-  @js(window, render($(col(removed, 1)), $(col(inserted,2)), $(col(inserted,3)), $(col(inserted,4)), $(col(inserted,5)), $(col(style,1)), $(col(style,2)), $(col(style,3)), $(col(text,1)), $(col(text,2)), $(col(onclick,1)), $(col(onkeydown,1))))
+  @js_(window, render($(col(removed, 1)), $(col(inserted,2)), $(col(inserted,3)), $(col(inserted,4)), $(col(inserted,5)), $(col(style,1)), $(col(style,2)), $(col(style,3)), $(col(text,1)), $(col(text,2)), $(col(onclick,1)), $(col(onkeydown,1))))
 end
 
 function render(window) 
@@ -160,7 +126,7 @@ function render(window)
     '
   """
   (inserted, style, text, onclick, onkeydown) = @show lb(command)
-  @js(window, render($[], $(col(inserted,2)), $(col(inserted,3)), $(col(inserted,4)), $(col(inserted,5)), $(col(style,1)), $(col(style,2)), $(col(style,3)), $(col(text,1)), $(col(text,2)), $(col(onclick,1)), $(col(onkeydown,1))))
+  @js_(window, render($[], $(col(inserted,2)), $(col(inserted,3)), $(col(inserted,4)), $(col(inserted,5)), $(col(style,1)), $(col(style,2)), $(col(style,3)), $(col(text,1)), $(col(text,2)), $(col(onclick,1)), $(col(onkeydown,1))))
 end
 
 function window()
@@ -176,6 +142,182 @@ function window()
   @js(window, document.body.innerHTML = $innerHTML)
   render(window)
   window
+end
+
+begin 
+  # dummy block for easy livecoding. we want upsertblock!
+  command = """
+    open imp
+    
+    addblock --name minesweeper '
+      foo[] = d -> 
+        int(d).
+    '
+  """
+  lb(command)
+end
+
+begin 
+  command = """
+    open imp 
+    
+    removeblock minesweeper
+    addblock --name minesweeper '
+      state[] = s ->
+        string(s).
+        
+      num_x[] = c ->
+        int(c).
+        
+      num_y[] = c ->
+        int(c).
+      
+      cell(id; x,y) ->
+        string(id), int(x), int(y).
+      
+      mine(x,y) ->
+        int(x), int(y).
+      
+      mine_count[x,y] = c ->
+        int(x), int(y), int(c).
+      
+      cleared(x,y) ->
+        int(x), int(y).
+        
+      num_cleared[] = c ->
+        int(c).
+        
+      num_mines[] = c ->
+        int(c).
+        
+      mine_x(i; x) -> 
+        int(i), int(x).
+        
+      mine_x(i; x) <- 
+        series<< x = rnd_uniform_int<1, 1000000000, 321>[i] >>
+        int:range(1, num_mines[], 1, i).
+        
+      mine_y(i; y) -> 
+        int(i), int(y).
+          
+      mine_y(i; y) <- 
+        series<< y = rnd_uniform_int<1, 1000000000, 123>[i] >>
+        int:range(1, num_mines[], 1, i).
+        
+      mine(x,y) <-
+        int:range(1, num_mines[], 1, i),
+        mine_x(i;xx),
+        mine_y(i;yy),
+        x = int:mod[xx, num_x[]],
+        y = int:mod[yy, num_y[]].
+      
+      cell(id;x,y) <-
+        int:range(1,num_x[],1,x),
+        int:range(1,num_y[],1,y),
+        id = "cell-" + int:string:convert[x] + "-" + int:string:convert[y].
+        
+      neighbouring_mine(x,y,nx,ny) <-
+        cell(_;x,y),
+        int:range(x-1,x+1,1,nx),
+        int:range(y-1,y+1,1,ny),
+        boolean:or(int:ne_3[nx,x],int:ne_3[ny,y], true),
+        mine(nx,ny).
+      
+      mine_count[x,y] = c <-
+        agg<< c=count() >>
+        cell(_;x,y),
+        neighbouring_mine(x,y,nx,ny).
+        
+      mine_count[x,y] = 0 <-
+        cell(_;x,y),
+        !neighbouring_mine(x,y,_,_).
+        
+      +cleared(x,y) <-
+        +click(id),
+        cell(id;x,y).
+        
+      +cleared(nx,ny) <-
+        +cleared(x,y),
+        mine_count(x,y;0),
+        int:range(x-1,x+1,1,nx),
+        int:range(y-1,y+1,1,ny),
+        boolean:or(int:ne_3[nx,x],int:ne_3[ny,y], true),
+        cell(_;nx,ny).
+        
+      num_cleared[] = c <-
+        agg<< c=count() >>
+        cleared(x,y).
+        
+      ^state[] = "game lost" <-
+        +click(id),
+        cell(id;x,y),
+        mine(x,y).
+        
+      ^state[] = "game won" <-
+        +click(id),
+        cell(id;x,y),
+        !mine(x,y),
+        !state@prev[] = "game lost",
+        num_cleared[] + num_mines[] = num_x[] * num_y[].
+        
+      node("grid"; "root", 1, "div"),
+      style("grid", "display"; "flex"),
+      style("grid", "flex-direction"; "column") <- .
+      
+      node(id; "grid", x, "div"),
+      style(id, "display"; "flex"),
+      style(id, "flex-direction"; "row") <-
+        cell(_;x,_),
+        id = "row-" + int:string:convert[x].
+        
+      node(id; parent, y, "button"),
+      style(id, "width"; "2em"),
+      style(id, "height"; "2em"),
+      onclick(id) <-
+        cell(id;x,y),
+        parent = "row-" +  int:string:convert[x].
+      
+      text[id] = "_" <-
+        cell(id;x,y),
+        state[] = "game in progress",
+        cleared(x,y),
+        mine_count[x,y] = 0.
+        
+      text[id] = int:string:convert[c] <-
+        cell(id;x,y),
+        state[] = "game in progress",
+        cleared(x,y),
+        mine_count[x,y] = c,
+        c != 0.
+        
+      text[id] = "M" <-
+        cell(id;x,y),
+        state[] = "game won",
+        mine(x,y).
+        
+      text[id] = "*" <-
+        cell(id;x,y),
+        state[] = "game lost",
+        mine(x,y).
+        
+      text[id] = int:string:convert[c] <-
+        cell(id;x,y),
+        state[] != "game in progress",
+        !mine(x,y),
+        mine_count[x,y] = c.
+    '
+    
+    exec '
+      ^state[] = "game in progress".
+      
+      ^num_x[] = 30.
+      ^num_y[] = 30.
+      ^num_mines[] = 90.
+    '
+    
+    query '_(x,y,c) <- mine_count(x,y,c).'
+  """
+  lb(command)
 end
 
 w = window()

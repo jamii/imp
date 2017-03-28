53b758400e4efd12594cd58425d40f183a70ca64
Jamie Brandon <jamie@scattered-thoughts.net>
2017-03-28 17:20:42 +0700

    Stash LB code generation

--- lb/lb.jl
+++ lb/lb.jl
@@ -1,6 +1,8 @@
 module LB
 
 using Match
+
+# --- ast ---
   
 immutable Var 
   name::Symbol
@@ -27,10 +29,12 @@ immutable FixedNode <: Node
 end
 
 immutable QueryNode <: Node
-  node::FixedNode
+  children::Vector{Node}
   table::Symbol
   vars::Vector{Var}
 end
+
+# --- parsing ---
 
 function preprocess(expr)
   @match expr begin
@@ -42,14 +46,36 @@ function preprocess(expr)
   end
 end
 
+function parse_nodes(exprs)
+  map(parse_node, exprs)
+end
+
 function parse_node(expr) 
   @match expr begin
+    text::Value => TextNode(text)
+    Expr(:call, [table::Symbol, children_expr, var_exprs...], _) => begin
+      @match children_expr begin
+        Expr(:->, [Expr(:tuple, [], _), Expr(:block, children_exprs, _)], _) => begin
+          children = parse_nodes(filter((e) -> !is_line_number(e), children_exprs))
+          vars = map(parse_var, var_exprs)
+          QueryNode(children, table, vars)
+        end
+        _ => error("What are this? $children_expr")
+      end
+    end
     [tag::Symbol, args...] => begin
       (attributes, more_args) = parse_attributes(args)
       nodes = parse_nodes(more_args)
       FixedNode(tag, attributes, nodes)
     end
     _ => error("What are this? $expr")
+  end
+end
+
+function is_line_number(expr)
+  @match expr begin
+    Expr(:line, _, _) => true
+    _ => false
   end
 end
 
@@ -63,14 +89,6 @@ function parse_attributes(exprs)
       end
       _ => return (attributes, exprs)
     end
-  end
-end
-
-function parse_nodes(exprs)
-  @match exprs begin
-    [text::Value] => [TextNode(text)]
-    [Expr(:call, [table::Symbol, vars...], _), child] => [QueryNode(parse_node(child), table, map(parse_var, vars))]
-    _ => map(parse_node, exprs)
   end
 end
   
@@ -88,37 +106,7 @@ function parse_dom(expr)
   end
 end
 
-function walk(node, bound_vars, parent_id, child_pos, output)
-  node_id = "$(parent_id)_$(child_pos)"
-  @match node begin
-    TextNode(text) => nothing # handled in generate_node on the parent
-    FixedNode(tag, attributes, children) => begin
-      generate_node(node, nothing, parent_id, node_id, child_pos, bound_vars, Var[], output)
-      for (i, child) in enumerate(children)
-        walk(child, bound_vars, node_id, i, output)
-      end
-    end
-    QueryNode(FixedNode(tag, attributes, children), table, vars) => begin
-      free_vars = Var[var for var in vars if !(var in bound_vars)]
-      generate_node(node.node, node, parent_id, node_id, child_pos, bound_vars, free_vars, output)
-      new_bound_vars = vcat(bound_vars, free_vars)
-      for (i, child) in enumerate(children)
-        walk(child, new_bound_vars, node_id, i, output)
-      end
-    end
-  end
-end
-
-function walk(dom)
-  output = String[]
-  walk(dom, Var[Var(:session, :string)], "lb", 0, output)
-  
-  unshift!(output, 
-    "id_lb[session] = id -> string(session), string(id).", 
-    "id_lb[session] = \"root\" <- dom:session(session).")
-  
-  join(output, "\n\n")
-end
+# --- codegen ---
 
 function var_string(var::Var) 
   @match var.typ begin
@@ -135,56 +123,110 @@ function val_string(val::Value)
   end
 end
 
-function generate_node(node, query, parent_id, node_id, child_pos, bound_vars, free_vars, output)
-  bound_var_names = join(["$(var.name)" for var in bound_vars], ",")
-  free_var_names = join(["$(var.name)" for var in free_vars], ",")
+# fixed/query is nearest FixedNode/QueryNode ancestor
+function generate(node, id, fixed_id, query_id, fixed_vars, query_vars, output)
+  vars = @match node begin
+    _::QueryNode => vcat(query_vars, [var for var in node.vars if !(var in query_vars)])
+    _ => query_vars
+  end
   
-  vars = vcat(bound_vars, free_vars)
   var_names = join(["$(var.name)" for var in vars], ",")
   var_types = join(["$(var.typ)($(var.name))" for var in vars], ",")
   var_strings = join([var_string(var) for var in vars], " + \"/\" + ")
+  fixed_var_names = join(["$(var.name)" for var in fixed_vars], ",")
+  fixed_var_types = join(["$(var.typ)($(var.name))" for var in fixed_vars], ",")
+  query_var_names = join(["$(var.name)" for var in query_vars], ",")
   
-  query_string = @match query begin
-    QueryNode(_, table, vars) => string(Expr(:call, table, [var.name for var in vars]...), ",")
-    _ => ""
+  @match node begin
+    _::TextNode => begin
+      # TODO can we make a text node in the dom instead of using dom:text?
+      text_data = "dom:text[session, id] = $(val_string(node.text)) <-\n    id_$(fixed_id)[$(fixed_var_names)] = id"
+      push!(output, text_data)
+      return "$(start) + 1"
+    end
+    _::QueryNode => begin
+      query_string = string(Expr(:call, table, [var.name for var in vars]...))
+      query_type = "data_$(id)[$(var_names)] ->\n    $(var_types)."
+      query_data = "data_$(id)[$(var_names)] <-\n    $(query_string), data_$(query_id)[$(query_var_names)] = _."
+      push!(output, query_type, query_data)
+      for (i, child) in enumerate(node.children)
+        generate(child, "$(id)_$(i)", fixed_id, id, fixed_vars, vars, output)
+      end
+    end
+    _::FixedNode => begin
+      id_type = "id_$(id)[$(var_names)] = id ->\n    $(var_types), string(id)."
+      id_data = "id_$(id)[$(var_names)] = \"$(id)/\" + $(var_strings) <-\n    data_$(query_id)[$(query_var_names)] = _."
+      parent_data = "dom:parent[session, child] = parent <-\n    id_$(id)[$(var_names)] = child, id_$(fixed_id)[$(fixed_var_names)] = parent."
+
+      node_data = "dom:node(session, id)"
+      tag_data = "dom:tag[session, id] = \"$(node.tag)\""
+      attribute_data = ["dom:attribute[session, id, $(val_string(attribute.key))] = $(val_string(attribute.val))" for attribute in node.attributes]
+      dom_data_head = join(vcat([node_data], [tag_data], attribute_data), ",\n")
+      dom_data = "$(dom_data_head) <-\n    id_$(id)[$(var_names)] = id."
+      
+      push!(output, id_type, id_data, parent_data, dom_data)
+      
+      for (i, child) in enumerate(node.children)
+        generate(child, "$(id)_$(i)", id, query_id, vars, query_vars, output)
+      end
+    end
   end
+end
+
+# TODO figure out positioning! 
+# hand on, maybe threading last does work?
+# parent calculates position of children
+# fixed node pass start = last(prev) + 1, sets position for fixed children, returns last = start + 1
+# query node positions each fixed child at start + mult * row + offset, returns last = start + num_children * num_rows
+
+function generate(node)
+  output = String[]
   
-  id_type = "id_$(node_id)[$(var_names)] = id ->\n    $(var_types), string(id)."
-  id_data = "id_$(node_id)[$(var_names)] = \"$(node_id)/\" + $(var_strings) <-\n    $(query_string) id_$(parent_id)[$(bound_var_names)] = _."
-  push!(output, id_type, id_data)
+  generate(node, "lb", "lb", "lb", Var[Var(:session, :string)], Var[Var(:session, :string)], output)
   
-  parent = "dom:parent[session, child] = parent <-\n    id_$(node_id)[$(var_names)] = child, id_$(parent_id)[$(bound_var_names)] = parent."
+  unshift!(output, 
+    "data_lb[session] -> string(session)",
+    "data_lb[session] <- dom:session(session)",
+    "id_lb[session] = id -> string(session), string(id).", 
+    "id_lb[session] = \"root\" <- dom:session(session).")
+  
+  join(output, "\n\n")
+end
+  
   push!(output, parent)
   
   @match query begin
     QueryNode(_, _, _) => begin
       position_var_names = join(["$(var.name)" for var in vcat(bound_vars, [Var(:pos, :int)], free_vars, [Var(:id, :int)])], ",")
-      position_type = "position_$(node_id)($(position_var_names)) ->\n    $(var_types), int(pos), string(id)."
-      position_data = "position_$(node_id)($(position_var_names)) <-\n    seq<<>> id_$(node_id)[$(var_names)] = id."
-      position = "dom:position[session, id] = pos <-\n    position_$(node_id)($(position_var_names))."
+      position_type = "position_$(id)($(position_var_names)) ->\n    $(var_types), int(pos), string(id)."
+      position_data = "position_$(id)($(position_var_names)) <-\n    seq<<>> id_$(id)[$(var_names)] = id."
+      position = "dom:position[session, id] = pos <-\n    position_$(id)($(position_var_names))."
       push!(output, position_type, position_data, position)
     end
     _ => begin
-      position = "dom:position[session, id] = $(child_pos-1) <-\n    id_$(node_id)[$(var_names)] = id."
+      position = "dom:position[session, id] = $(child_pos-1) <-\n    id_$(id)[$(var_names)] = id."
       push!(output, position)
     end
   end
   
-  node_data = ["dom:node(session, id)"]
-  tag_data = ["dom:tag[session, id] = \"$(node.tag)\""]
-  attribute_data = ["dom:attribute[session, id, $(val_string(attribute.key))] = $(val_string(attribute.val))" for attribute in node.attributes]
-  text_data = @match node.children begin
-    [TextNode(text)] => ["dom:text[session, id] = $(val_string(text))"]
-    _ => []
-  end
-  dom_data_head = join(vcat(node_data, tag_data, attribute_data, text_data), ",\n")
-  dom_data = "$(dom_data_head) <-\n    id_$(node_id)[$(var_names)] = id."
+  
   push!(output, dom_data)
 end
 
 function compile(expr)
-  walk(parse_dom(expr))
+  generate(parse_dom(expr))
 end
+
+# --- demos ---
+
+# TODO 
+# add extra positions so that can have multiple children
+# figure out how to handle styles (maybe k:v)
+
+# ALTERNATIVE
+# generate code to create an api
+# treat as tree of query nodes, mapped to dom
+# diff query tree and patch up dom
 
 chat = 
 """
@@ -256,25 +298,21 @@ block(`chat) {
     
 $(compile(quote
   [div
-    [div
-      login(session:string)
+    login(session:string) do
       [input "type"="text" "placeholder"="What should we call you?"]
-    ]
-    [div
-      chat(session:string)
+    end
+    
+    chat(session:string) do
       [div
-        [div
-          message(message:int, text:string, time:string)
+        message(message:int, text:string, time:string) do
           [div
             [span "class"="message-time" time]
             [span "class"="message-text" text]
           ]
-        ]
-        [div 
-          [input "type"="text" "placeholder"="What do you want to say?"]
-        ]
+        end
       ]
-    ]
+      [input "type"="text" "placeholder"="What do you want to say?"]
+    end
   ]
 end))
     
@@ -282,28 +320,28 @@ end))
 } <-- .
 """
 
-# [div
-#   login(session:string) {
-#     [input $(:type)="text" placeholder="What should we call you?"]
-#   }
-#   
-#   chat(session:string) {
-#     [div
-#       message(message:int, text:string, time:string) {
-#         [div
-#           [span class="message-time" time]
-#           [span class="message-text" text]
-#         ]
-#       }
-#     ]
-#     [input $(:type)="text" placeholder="What do you want to say?"]
-#   }
-# ]
-
 open("lb/demo/chat.logic", "w") do f
   write(f, chat)
 end
 
 run(`bash ./lb/build.sh`)
+
+  [div
+    login(session:string) do
+      [input "type"="text" "placeholder"="What should we call you?"]
+    end
+    
+    chat(session:string) do
+      [div
+        message(message:int, text:string, time:string) do
+          [div
+            [span "class"="message-time" time]
+            [span "class"="message-text" text]
+          ]
+        end
+      ]
+      [input "type"="text" "placeholder"="What do you want to say?"]
+    end
+  ]
 
 end

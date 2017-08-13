3d76a6a558a8379380cce58f1d74cc458ee406c3
Jamie Brandon <jamie@scattered-thoughts.net>
2017-08-13 10:36:44 +0100

    Back to rust

--- lb/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# hacky mess
-source ~/logicblox/etc/profile.d/logicblox.sh
-source ~/logicblox/etc/bash_completion.d/logicblox.sh
-lb compile project ~/imp/lb/imp.project && lb delete imp && lb create imp && lb addproject imp ~/imp/lb
-sudo pkill -9 nginx
-sudo nginx -c ~/imp/lb/nginx.conf -p ~/imp/lb

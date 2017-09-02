bc6f486fc8911fec0c104562bbc53bdc2d2f8992
Jamie Brandon <jamie@scattered-thoughts.net>
2017-09-02 08:27:36 +0100

    Basic editing and persistence

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ extern crate websocket;
 extern crate serde;
 #[macro_use(json, json_internal)]
 extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate pulldown_cmark;
 
-use maud::html;
+use maud::{html, PreEscaped};
+use pulldown_cmark::{Parser, html};
 
 use serde::Serialize;
 use serde_json::{Value, Error};
@@ -17,6 +21,15 @@ use std::sync::{Arc, Mutex};
 use websocket::OwnedMessage;
 use websocket::sync::Server;
 
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct Note {
+    text: String,
+    editing: bool,
+}
+
 fn command<Kind: Serialize, Args: Serialize>(kind: Kind, args: Args) -> OwnedMessage {
     OwnedMessage::Text(
         json!({
@@ -26,26 +39,47 @@ fn command<Kind: Serialize, Args: Serialize>(kind: Kind, args: Args) -> OwnedMes
     )
 }
 
-fn render(notes: &[String]) -> String {
+fn render(notes: &[Note]) -> String {
     (html!{
         div.notes {
             button onclick="message('new_note', [])" "+"
-            @for note in notes {
-                div.note (note)
+            @for (i, note) in notes.iter().enumerate() {
+                @if note.editing {
+                    textarea.edit onblur={"message('finish', [" (i) ", this.value])"} onkeydown={"if (event.which == 13 && event.ctrlKey) message('finish', [" (i) ", this.value]); if (event.which == 27) message('escape', [" (i) "]);"} (note.text)
+                } @else {
+                    div.note onclick={"message('edit', [" (i) "])"} ({
+                        let mut unsafe_html = String::new();
+                        let parser = Parser::new(note.text.as_str());
+                        html::push_html(&mut unsafe_html, parser);
+                        PreEscaped(unsafe_html)
+                    })
+                }
             }
         }
     }).into_string()
 }
 
+fn load() -> Vec<Note> {
+    let mut file = File::open("/home/jamie/imp.db").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    serde_json::from_str(&*contents).unwrap()
+}
+
+fn save(notes: &[Note]) {
+    let mut file = File::create("/home/jamie/imp.db").unwrap();
+    write!(file, "{}", json!(notes)).unwrap();
+}
+
 fn main() {
-    let notes = Arc::new(Mutex::new(vec!["First post!".to_owned()]));
+    let notes = Arc::new(Mutex::new(load()));
 
     let server = Server::bind("127.0.0.1:8080").unwrap();
 
     for request in server.filter_map(Result::ok) {
         let notes = notes.clone();
         thread::spawn(move || {
-            let mut client = request.accept().unwrap();
+            let client = request.accept().unwrap();
             let ip = client.peer_addr().unwrap();
             println!("Connection from {}", ip);
 
@@ -77,12 +111,34 @@ fn main() {
                         println!("Received: {}", text);
                         let json: Value = serde_json::from_str(text).unwrap();
                         match json["kind"].as_str().unwrap() {
-                            "new_note" => notes.lock().unwrap().push("New!".to_owned()),
-                            other => panic!("What is this message? {}", text),
+                            "new_note" => {
+                                notes.lock().unwrap().push(Note {
+                                    text: "New!".to_owned(),
+                                    editing: true,
+                                })
+                            }
+                            "finish" => {
+                                let i = json["args"][0].as_u64().unwrap() as usize;
+                                let new_text = json["args"][1].as_str().unwrap();
+                                notes.lock().unwrap()[i] = Note {
+                                    text: new_text.to_owned(),
+                                    editing: false,
+                                };
+                            }
+                            "edit" => {
+                                let i = json["args"][0].as_u64().unwrap() as usize;
+                                notes.lock().unwrap()[i].editing = true;
+                            }
+                            "escape" => {
+                                let i = json["args"][0].as_u64().unwrap() as usize;
+                                notes.lock().unwrap()[i].editing = false;
+                            }
+                            _ => panic!("What is this message? {}", text),
                         }
                         sender
                             .send_message(&command("render", (render(&*notes.lock().unwrap()),)))
                             .unwrap();
+                        save(&*notes.lock().unwrap())
                     }
                     _ => {
                         panic!("A weird message!");

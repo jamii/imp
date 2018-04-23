95bb3f3512848008630933029abaecf1f353cf3c
Jamie Brandon <jamie@scattered-thoughts.net>
2018-04-23 21:48:33 +0100

    Reset

---
+++ src/Imp.jl
@@ -0,0 +1,25 @@
+module Imp
+
+import MacroTools
+import MacroTools: @capture
+using Rematch
+
+# TODO remove workaround for https://github.com/JuliaLang/julia/issues/26885
+function Base.show(io::IO, set::Set)
+    print(io, "Set(", collect(set), ")")
+end
+
+include("util.jl")
+include("semantics.jl")
+
+macro imp(ast)
+    parse(ast)
+end
+
+macro imp(env, ast)
+    :(interpret($(esc(env)), parse($(QuoteNode(ast)))))
+end
+
+export stdenv, @imp
+
+end

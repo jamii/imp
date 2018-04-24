80d7cbf81bf1d7c8f964024b9b6971c4bd5c43d2
Jamie Brandon <jamie@scattered-thoughts.net>
2018-04-24 14:30:00 +0100

    Move to single files

--- src/Imp.jl
+++ src/Imp.jl
@@ -9,9 +9,227 @@ function Base.show(io::IO, set::Set)
     print(io, "Set(", collect(set), ")")
 end
 
-include("util.jl")
-include("semantics.jl")
-include("inference.jl")
+macro splice(iterator, body)
+  @assert iterator.head == :call
+  @assert iterator.args[1] == :in
+  Expr(:..., :(($(esc(body)) for $(esc(iterator.args[2])) in $(esc(iterator.args[3])))))
+end
+
+# --- parse ---
+
+abstract type Expr end
+
+struct Constant{T} <: Expr
+    value::T # scalar
+end
+
+struct Var <: Expr
+    name::Symbol
+end
+
+struct Apply <: Expr
+    f::Expr
+    args::Vector{Expr}
+end
+
+struct Abstract <: Expr
+    variable::Symbol
+    value::Expr
+end
+
+# things which operate on relations rather than on values
+struct Primitive <: Expr
+    f::Symbol
+    args::Vector{Expr}
+end
+
+function parse(ast)
+    if @capture(ast, constant_Int64_String_Bool)
+        Constant(constant)
+    elseif @capture(ast, name_Symbol)
+        Var(name)
+    elseif @capture(ast, f_(args__)) && length(args) > 0
+        if f in [:|, :&, :!, :(=>), :(==), :reduce, :exists, :forall]
+            Primitive(f, map(parse, args))
+        else
+            Apply(parse(f), map(parse, args))
+        end
+    elseif @capture(ast, if cond_ true_branch_ end)
+        Primitive(:iff, [parse(cond), parse(true_branch), Constant(false)])
+    elseif @capture(ast, if cond_ true_branch_ else false_branch_ end)
+        Primitive(:iff, [parse(cond), parse(true_branch), parse(false_branch)])
+    elseif @capture(ast, (variable_Symbol) -> value_)
+        Abstract(variable, parse(value))
+    else
+        error("Unknown syntax: $ast")
+    end
+end
+
+function unparse(expr)
+    @match expr begin
+        Constant(value) => value
+        Var(name) => name
+        Apply(f, args) => :($(unparse(f))($(map(unparse, args)...)))
+        Abstract(variable, value) => :(($variable) -> $(unparse(value)))
+        Primitive(:iff, [cond, true_branch, Constant(false)]) => :(if $(unparse(cond)) $(unparse(true_branch)) end)
+        Primitive(:iff, [cond, true_branch, false_branch]) => :(if $(unparse(cond)) $(unparse(true_branch)) else $(unparse(false_branch)) end)
+        Primitive(f, args) => :($(unparse(f))($(map(unparse, args)...)))
+    end
+end
+
+# --- interpret ---
+
+const Env = Dict{Symbol, Set}
+
+const truthy = Set([()])
+const falsey = Set()
+bool_to_set(bool::Bool)::Set = bool ? truthy : falsey
+set_to_bool(set::Set)::Bool = length(set) > 0
+
+function interpret(env::Env, expr::Constant) ::Set
+    Set([(expr.value,)])
+end
+
+function interpret(env::Env, expr::Constant{Bool}) ::Set
+    bool_to_set(expr.value)
+end
+
+function interpret(env::Env, expr::Var) ::Set
+    env[expr.name]
+end
+
+function interpret(env::Env, expr::Apply) ::Set
+    f = interpret(env, expr.f)
+    for arg in map((arg) -> interpret(env, arg), expr.args)
+        result = Set()
+        for n in map(length, arg)
+            for row in f
+                if (length(row) >= n) && (row[1:n] in arg)
+                    push!(result, row[n+1:end])
+                end
+            end
+        end
+        f = result
+    end
+    f
+end
+
+function interpret(env::Env, expr::Abstract) ::Set
+    env = copy(env)
+    result = Set()
+    for domain_row in env[:everything]
+        env[expr.variable] = Set([domain_row])
+        value = interpret(env, expr.value)
+        for value_row in value
+            push!(result, (domain_row..., value_row...))
+        end
+    end
+    result
+end
+
+function interpret(env::Env, expr::Primitive) ::Set
+    args = [interpret(env, arg) for arg in expr.args]
+    @match (expr.f, args) begin
+        (:|, _) => union(args...)
+        (:&, _) => intersect(args...)
+        (:!, [arg]) => bool_to_set(!set_to_bool(arg))
+        (:(=>), [a, b]) => bool_to_set((!set_to_bool(a) || set_to_bool(b)))
+        (:(==), _) => bool_to_set((==)(args...))
+        (:iff, [cond, true_branch, false_branch]) => set_to_bool(cond) ? true_branch : false_branch
+        (:reduce, [raw_op, raw_init, values]) => begin
+            op = Dict(((a,b) => c for (a,b,c) in raw_op))
+            @assert length(raw_init) == 1
+            @assert length(first(raw_init)) == 1
+            init = first(raw_init)[1]
+            value = reduce((a,b) -> op[a,b[end]], init, values)
+            Set([(value,)])
+        end
+        (:exists, [arg]) => bool_to_set(arg != env[:nothing])
+        (:forall, [arg]) => bool_to_set(arg == env[:everything])
+        _ => error("Unknown primitive: $expr")
+    end
+end
+
+stdenv = Dict{Symbol, Set}(
+  :everything => Set{Any}([(scalar,) for scalar in [0, 1, 2, "alice", "bob", "eve", "cthulu", "yes", "no"]]),
+  :nothing => Set([]),
+)
+
+# --- infer ---
+
+const RowType = NTuple{N, Type} where N
+const SetType = Set{RowType}
+const EnvTypes = Dict{Symbol, SetType}
+
+const universe_types = Set([Int64, Float64, String])
+const bool_type = SetType([()])
+const false_type = SetType([])
+
+function infer(env_types::EnvTypes, expr::Constant{T})::SetType where T
+    Set([(T,)])
+end
+
+function infer(env_types::EnvTypes, expr::Constant{Bool})::SetType where T
+    expr.value ? bool_type : false_type
+end
+
+function infer(env_types::EnvTypes, expr::Var)::SetType
+    env_types[expr.name]
+end
+
+function infer(env_types::EnvTypes, expr::Apply)::SetType
+    f_type = infer(env_types, expr.f)
+    for arg_type in map((arg) -> infer(env_types, arg), expr.args)
+        result_type = SetType()
+        for f_row_type in f_type
+            for arg_row_type in arg_type
+                if length(arg_row_type) <= length(f_row_type) &&
+                    arg_row_type == f_row_type[1:length(arg_row_type)]
+                    push!(result_type, f_row_type[length(arg_row_type)+1:end])
+                end
+            end
+        end
+        f_type = result_type
+    end
+    f_type
+end
+
+function infer(env_types::EnvTypes, expr::Abstract)::SetType
+    result_type = SetType()
+    env_types = copy(env_types)
+    for variable_type in universe_types
+        env_types[expr.variable] = SetType([(variable_type,)])
+        for value_type in infer(env_types, expr.value)
+            push!(result_type, (variable_type, value_type...))
+        end
+    end
+    result_type
+end
+
+function infer(env_types::EnvTypes, expr::Primitive)::SetType
+    arg_types = [infer(env_types, arg) for arg in expr.args]
+    @match (expr.f, arg_types) begin
+        (:|, _) => union(arg_types...)
+        (:&, _) => intersect(arg_types...)
+        (:!, [arg]) => bool_type
+        (:(=>), [a, b]) => bool_type
+        (:(==), _) => intersect(arg_types...) == false_type ? false_type : bool_type
+        (:iff, [cond_type, then_type, else_type]) => begin
+            # TODO if we had lower bounds on cond we could also do then_type when it's definitely true 
+            if cond_type == false_type
+                else_type
+            else
+                union(then_type, else_type)
+            end
+        end
+        (:reduce, [raw_op, raw_init, values]) => map(row_type -> (row_type[3],), raw_op)
+        (:exists, [arg]) => bool_type
+        (:forall, [arg]) => bool_type
+        _ => error("Unknown primitive: $expr")
+    end
+end
+
+# --- exports ---
 
 macro imp(ast)
     parse(ast)
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,11 +15,153 @@ env = merge(stdenv, Dict{Symbol, Set}(
     :points => Set([("alice", 0), ("bob", 1), ("cthulu", 1)]),
 ))
 
-@testset "semantics" begin
-    include("test_semantics.jl")
+# --- interpret ---
+
+macro test_imp(ast)
+    @assert @capture(ast, left_ == right_)
+    # TODO not sure why these don't need escaping...
+    :(@test @imp($env, $left) == @imp($env, $right))
 end
-@testset "inference" begin 
-    include("test_inference.jl")
+
+@test Imp.set_to_bool(Imp.bool_to_set(true)) == true
+@test Imp.set_to_bool(Imp.bool_to_set(false)) == false
+
+# basic booleans
+@test @imp(env, true) == Set([()])
+@test @imp(env, false) == Set()
+@test_imp (true | true) == true
+@test_imp (true | false) == true
+@test_imp (false | true) == true
+@test_imp (false | false) == false
+@test_imp (true & true) == true
+@test_imp (true & false) == false
+@test_imp (false & true) == false
+@test_imp (false & false) == false
+@test_imp !true == false
+@test_imp !false == true
+
+# 'implicit' boolean
+@test_imp person("alice") == true
+@test_imp person(0) == false
+@test_imp person == "alice" | "bob" | "eve"
+
+# three-valued logic
+@test_imp rsvp("alice") == "yes"
+@test_imp rsvp("bob") == "no"
+@test_imp rsvp("eve") == nothing
+
+# convert value to boolean
+@test_imp ("yes" == "yes") == true
+@test_imp ("yes" == "no") == false
+@test_imp (rsvp("alice") == "yes") == true
+@test_imp (rsvp("bob") == "yes") == false
+@test_imp (rsvp("eve") == "yes") == false
+@test_imp rsvp("alice", "yes") == true
+@test_imp rsvp("bob", "yes") == false
+@test_imp rsvp("eve", "yes") == false
+
+# convert boolean to value
+# (`if` is taken so `iff`)
+@test_imp if true "yes" else "no" end == "yes"
+@test_imp if false "yes" else "no" end == "no"
+@test_imp (true ? "yes" : "no") == "yes"
+@test_imp (false ? "yes" : "no") == "no"
+
+# abstraction
+@test_imp (x -> true) == everything
+@test_imp (x -> false) == nothing
+@test_imp (p -> person(p)) == person
+@test_imp (x -> "alice")(2) == "alice"
+
+# domain expressed via `iff`
+@test_imp (p -> if person(p) rsvp(p) end)("alice") == "yes"
+@test_imp (p -> if person(p) rsvp(p) end)("cthulu") == false
+@test_imp (p -> if person(p) rsvp(p, "yes") end) == "alice"
+@test_imp (p -> if person(p) rsvp(p, "no") end) == "bob"
+@test_imp (p -> rsvp(p, "no")) == ("bob" | "cthulu")
+
+# defaults via `iff`
+@test_imp (p -> if person(p) rsvp(p) else "n/a" end)("alice") == "yes"
+@test_imp (p -> if person(p) rsvp(p) else "n/a" end)("cthulu") == "n/a"
+@test_imp (p -> if person(p) rsvp(p) else "n/a" end)(2) == "n/a"
+@test_imp (p -> if person(p) rsvp(p) else (if string(p) "n/a" end) end)("cthulu") == "n/a"
+@test_imp (p -> if person(p) rsvp(p) else (if string(p) "n/a" end) end)(2) == false
+
+# can do quantification via eq
+
+# exists == X :: not(eq(X, nothing))
+@test_imp exists(person) == true
+@test_imp exists(nothing) == false
+
+# forall == X :: eq(X, everything)
+@test_imp forall(p -> person(p) => string(p)) == true
+@test_imp forall(p -> person(p) => evil(p)) == false
+@test_imp forall(p -> person(p) => rsvp(p, "yes")) == false
+@test_imp forall(p -> person(p) => (rsvp(p, "yes") | rsvp(p, "no"))) == false
+@test_imp forall(p -> person(p) => (rsvp(p, "yes") | rsvp(p, "no") | "eve"(p))) == true
+
+# aggregation
+@test_imp reduce(+, 0, points) == 2
+
+# tricky expressions
+@test_imp (x -> x(x)) == everything
+
+# --- infer ---
+
+env_types = Dict{Symbol, Imp.SetType}((name => Imp.SetType([map(typeof, row) for row in set]) for (name, set) in env))
+
+macro test_infer(expr, typs...)
+    :(@test infer($env_types, @imp($expr)) == Imp.SetType([$(typs...)]))
 end
+
+@test_infer true ()
+@test_infer false
+@test_infer 1 (Int64,)
+@test_infer "yes" (String,)
+
+@test_infer (true | true) ()
+@test_infer (true & true) ()
+@test_infer !true ()
+
+@test_infer person("alice") ()
+@test_infer person(0) 
+@test_infer "alice" | "bob" | "eve" (String,)
+
+@test_infer rsvp("alice") (String,)
+
+@test_infer "yes" == "yes" ()
+@test_infer rsvp("alice") == "yes" ()
+@test_infer rsvp("alice", "yes") ()
+
+@test_infer if true "yes" else "no" end (String,)
+@test_infer if true "yes" else 0 end (Int64,) (String,)
+
+@test_infer (x -> true) (String,) (Int64,) (Float64,)
+@test_infer (p -> if person(p) rsvp(p) end) (String, String)
+@test_infer (p -> if person(p) rsvp(p) else (if string(p) "n/a" end) end) (String, String)
+@test_infer (p -> if person(p) rsvp(p) else (if string(p) "n/a" end) end)("cthulu") (String,)
+
+@test_infer exists(person) ()
+@test_infer forall(p -> person(p) => string(p)) ()
+
+@test_infer reduce(+, 0, points) (Int64,)
+
+# harder cases
+
+@test_infer (p -> p == 0) (Int64,)
+@test_infer (p -> (p == 0) | string(p)) (Int64,) (String,)
+@test_infer (p -> (p == 0) | (string(p) & integer(p))) (Int64,)
+@test_infer (p -> if person(p) 0 else (if integer(p) "yes" end) end) (String, Int64) (Int64, String)
+@test_infer (p -> if person(p) 0 else (if p == 0 "yes" end) end) (String, Int64) (Int64, String)
+
+@test_infer (x -> (y -> (string(x) & string(y)) | (integer(x) & integer(y)))) (String, String) (Int64, Int64)
+@test_infer (x -> (y -> (person(x) & person(y)) | (x + y == 0))) (String, String) (Int64, Int64)
+
+# TODO need a way to represent true_type
+# TODO need to know that string(x) is always true if x::String
+# @test_infer (x -> if person(x); (if string(x) "yes" else 0 end) end) (String, String)
+
+# TODO requires reasoning with sets as bounds rather than just types
+# @test_infer (x -> if person(x); (if person(x) "yes" else 0 end) end) (String, String)
 
 end

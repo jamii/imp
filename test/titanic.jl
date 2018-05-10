0e0db30190d0f8e54e1a88d5f0ba191c44af6814
Jamie Brandon <jamie@scattered-thoughts.net>
2018-05-10 11:41:40 +0100

    Load titanic data

---
+++ test/titanic.jl
@@ -0,0 +1,12 @@
+module Titanic
+
+using Imp
+using CSV
+
+# NOTE
+# needs DataStreams master
+
+train = CSV.read("data/titanic/train.csv", header=1, types=[Int64, Int64, Int64, String, String, Union{Float64,Missing}, Int64, Int64, String, Float64, Union{String,Missing}, Union{String,Missing}])
+train.colindex
+
+end

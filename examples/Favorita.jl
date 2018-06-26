2328b7286384b404e0602e7907542a55aa2c9fca
Jamie Brandon <jamie@scattered-thoughts.net>
2018-06-26 17:06:39 +0100

    Attempted prediction with MixedModels

--- examples/Favorita.jl
+++ examples/Favorita.jl
@@ -1,8 +1,8 @@
-module Favorita
+# module Favorita
 
 # using Imp
 
-# using JuliaDB
+using JuliaDB
 
 # @time begin
 #     holidays_events = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/holidays_events.csv")
@@ -10,7 +10,7 @@ module Favorita
 #     oil = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/oil.csv")
 #     stores = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/stores.csv")
 #     test = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/test.csv")
-#     train = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train-mini.csv")
+#     train = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train.csv")
 #     transactions = loadtable("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/transactions.csv")
 # end
 
@@ -20,17 +20,17 @@ module Favorita
 #     save(oil, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/oil.jdb")
 #     save(stores, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/stores.jdb")
 #     save(test, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/test.jdb")
-#     save(train, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train-mini.jdb")
+#     save(train, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train.jdb")
 #     save(transactions, "/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/transactions.jdb")
 # end
 
 # @time begin
-#     @show holidays_events = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/holidays_events.jdb")
+#     holidays_events = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/holidays_events.jdb")
 #     items = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/items.jdb")
 #     oil = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/oil.jdb")
 #     stores = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/stores.jdb")
 #     test = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/test.jdb")
-#     @show train = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train-mini.jdb")
+#     train = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train.jdb")
 #     transactions = load("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/transactions.jdb")
 # end
 
@@ -53,21 +53,21 @@ Categorical = CSV.CategoricalArrays.CategoricalString{UInt32}
     oil = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/oil.csv", types=[Date, Union{Missing, Float64}], dateformat="yyyy-mm-dd")
     stores = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/stores.csv", types=[Int64, Categorical, Categorical, Categorical, Int8])
     test = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/test.csv", types=[Int64, Date, Int8, Int64, Union{Bool, Missing}], truestring="True", falsestring="False", dateformat="yyyy-mm-dd")
-    train = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train-mini.csv", types=[Int64, Date, Int8, Int64, Float64, Union{Bool, Missing}], truestring="True", falsestring="False", dateformat="yyyy-mm-dd")
+    train = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/train.csv", types=[Int64, Date, Int8, Int64, Float64, Union{Bool, Missing}], truestring="True", falsestring="False", dateformat="yyyy-mm-dd")
     transactions = CSV.read("/home/jamie/.kaggle/competitions/favorita-grocery-sales-forecasting/transactions.csv", types=[Date, Int8, Int64], dateformat="yyyy-mm-dd")
 end
 
-@time begin
-    unique_holidays_events = by(holidays_events, :date, x -> x[1, 2:end])
+# @time begin
+#     unique_holidays_events = by(holidays_events, :date, x -> x[1, 2:end])
 
-    data = train
-    data = join(data, unique_holidays_events, on=[:date], kind=:left)
-    data = join(data, items, on=[:item_nbr])
-    data = join(data, oil, on=[:date], kind=:left)
-    data = join(data, stores, on=[:store_nbr], makeunique=true)
-    data = join(data, transactions, on=[:date, :store_nbr], kind=:left)
-    @assert size(data)[1] == size(train)[1]
-end
+#     data = train
+#     data = join(data, unique_holidays_events, on=[:date], kind=:left)
+#     data = join(data, items, on=[:item_nbr])
+#     data = join(data, oil, on=[:date], kind=:left)
+#     data = join(data, stores, on=[:store_nbr], makeunique=true)
+#     data = join(data, transactions, on=[:date, :store_nbr], kind=:left)
+#     @assert size(data)[1] == size(train)[1]
+# end
 
 # using Query
 
@@ -119,12 +119,35 @@ end
 
 # using OnlineStats
 
-# using MixedModels
+using MixedModels
 
-# model = fit(LinearMixedModel, @formula(unit_sales ~ (1|store_nbr) + (1|item_nbr)), train_data)
-# # "At a superficial level these can be considered as the "estimates" of the random effects, with a bit of hand waving, but pursuing this analogy too far usually results in confusion."
-# params = ranef(model, named=true)
+@time model = fit(LinearMixedModel, @formula(unit_sales ~ (1|store_nbr) + (1|item_nbr)), train_data)
+# "At a superficial level these can be considered as the "estimates" of the random effects, with a bit of hand waving, but pursuing this analogy too far usually results in confusion."
+params = ranef(model, named=true)
+
+function StatsBase.fitted(m::LinearMixedModel{T}, new_data) where T
+    ## FIXME: Create and use `effects(m) -> β, b` w/o calculating β twice
+    new_m = LinearMixedModel(m.formula, new_data)
+    v = Array{T}(nobs(new_m))
+    trms = new_m.trms
+    A_mul_B!(vec(v), trms[end - 1], fixef(m))
+    b = ranef(m)
+    for j in eachindex(b)
+        MixedModels.unscaledre!(vec(v), trms[j], b[j])
+    end
+    v
+end
+
+@time begin
+    actual = test_data[:unit_sales]
+    predicted = fitted(model, data)[!sample] # for some reason LinearMixedModel(formula, test_data) fails
+    predicted = map(x -> max(x, 0.0), predicted)
+    weight = map(p -> p ? 1.25 : 1.00, test_data[:perishable])
+    score = sqrt(sum(@. weight * (log(actual + 1) - log(predicted + 1)) ^ 2) / sum(weight))
+end
+
+@show score
 
 # using MultivariateStats
 
-end
+# end

cb436a32d65ab23676b5a5d2806e95bf72eaaaac
Jamie Brandon <jamie@scattered-thoughts.net>
2018-07-10 20:46:23 +0100

    Iter over factors instead of rows

--- src/first.jl
+++ src/first.jl
@@ -2,53 +2,31 @@ const LoHi = UnitRange{Int64}
 
 struct Factor{Columns <: NTuple{N, Vector} where N}
     columns::Columns # at least one column, at least one row
+    column_ix::Int64
     lohi::LoHi
 end
 
-Factor(columns) = Factor(columns, 1:length(columns[1]))
+Factor(columns) = Factor(columns, 1, 1:length(columns[1]))
 
 function factor_first(factor::Factor)::Tuple{Any, Factor}
-    value = factor.columns[1][1]
-    lohi = searchsorted(factor.columns[1], value)
-    (value, Factor(factor.columns, lohi))
+    value = factor.columns[factor.column_ix][1]
+    lohi = searchsorted(factor.columns[factor.column_ix], value)
+    (value, Factor(factor.columns, factor.column_ix, lohi))
 end
 
 function factor_next(factor::Factor)::Union{Tuple{Any, Factor}, Nothing}
-    factor.lohi.stop >= length(factor.columns[1]) && return nothing
-    value = factor.columns[1][factor.lohi.stop + 1]
-    lohi = searchsorted(factor.columns[1], value)
-    (value, Factor(factor.columns, lohi))
+    factor.lohi.stop >= length(factor.columns[factor.column_ix]) && return nothing
+    value = factor.columns[factor.column_ix][factor.lohi.stop + 1]
+    lohi = searchsorted(factor.columns[factor.column_ix], value)
+    (value, Factor(factor.columns, factor.column_ix, lohi))
 end
 
 function factor_down(factor::Factor)::Factor
-    @assert length(factor.columns) > 0
-    columns = tuple((column[factor.lohi] for column in factor.columns[2:end])...)
-    Factor(columns, 1:(factor.lohi.stop - factor.lohi.start + 1))
-end
-
-function foreach(f, factor::Factor)
-    for i in factor.lohi
-        row = tuple((column[i] for column in factor.columns)...)
-        f(row)
-    end
-end
-
-struct Product end
-
-function foreach(f, product::Product, factors)
-    if length(factors) == 0
-        f(())
-    else
-        foreach(factors[1]) do row_left
-            foreach(product, factors[2:end]) do row_right
-                f(tuple(row_left..., row_right...))
-            end
-        end
-    end
-end        
-
-struct MergeJoin{factor_ixes, Tail}
-    tail::Tail
+    @assert !isempty(factor.columns)
+    columns = tuple((column[factor.lohi] for column in factor.columns)...)
+    column_ix = factor.column_ix + 1
+    lohi = 1:(factor.lohi.stop - factor.lohi.start + 1)
+    Factor(columns, column_ix, lohi)
 end
 
 macro factor_next(f)
@@ -59,6 +37,38 @@ macro factor_next(f)
     end
 end
 
+# function foreach(f, factor::Factor)
+#     for i in factor.lohi
+#         row = tuple((column[i] for column in factor.columns)...)
+#         f(row)
+#     end
+# end
+
+struct MergeJoin1{factor_ixes} end
+
+MergeJoin1(factor_ixes) = MergeJoin1{factor_ixes}()
+
+function factors_merge(factors, (a, fa))
+    factors = Any[factors...]
+    factors[a] = fa
+    tuple(factors...)
+end
+
+function foreach(f, join::MergeJoin1{factor_ixes}, factors) where {factor_ixes}
+    (a,) = factor_ixes
+
+    (va, fa) = factor_first(factors[a])
+
+    while true
+        f(factors_merge(factors, a => factor_down(fa)))
+        (va, fa) = @factor_next(fa)
+    end
+end
+
+struct MergeJoin2{factor_ixes} end
+
+MergeJoin2(factor_ixes) = MergeJoin2{factor_ixes}()
+
 function factors_merge(factors, (a, fa), (b, fb))
     factors = Any[factors...]
     factors[a] = fa
@@ -66,7 +76,7 @@ function factors_merge(factors, (a, fa), (b, fb))
     tuple(factors...)
 end
 
-function foreach(f, join::MergeJoin{factor_ixes}, factors) where {factor_ixes}
+function foreach(f, join::MergeJoin2{factor_ixes}, factors) where {factor_ixes}
     (a, b) = factor_ixes
 
     (va, fa) = factor_first(factors[a])
@@ -75,9 +85,7 @@ function foreach(f, join::MergeJoin{factor_ixes}, factors) where {factor_ixes}
     while true
         c = cmp(va, vb)
         if c == 0
-            foreach(join.tail, factors_merge(factors, a => factor_down(fa), b => factor_down(fb))) do row
-                f((va, row...))
-            end
+            f(factors_merge(factors, a => factor_down(fa), b => factor_down(fb)))
             (va, fa) = @factor_next(fa)
             (vb, fb) = @factor_next(fb)
         elseif c == -1
@@ -88,17 +96,28 @@ function foreach(f, join::MergeJoin{factor_ixes}, factors) where {factor_ixes}
     end
 end
 
-function list(expr)
-    rows = []
-    foreach(expr) do row
-        push!(rows, row)
-    end
-    rows
+struct Chain
+    exprs::Tuple
 end
 
-function list(expr, factors)
+function foreach(f, chain::Chain, factors)
+    if isempty(chain.exprs)
+        f(factors)
+    else
+        foreach(chain.exprs[1], factors) do factors
+            foreach(f, Chain(chain.exprs[2:end]), factors)
+        end
+    end
+end
+
+function select(factor, ix)
+    factor.columns[ix][factor.lohi.start]
+end
+
+function select(expr, factors, ixes)
     rows = []
-    foreach(expr, factors) do row
+    foreach(expr, factors) do factors
+        row = tuple((select(factors[ix[1]], ix[2]) for ix in ixes)...)
         push!(rows, row)
     end
     rows
@@ -107,7 +126,20 @@ end
 dep = Factor((["Eng", "Eng", "Sec"], ["Alice", "Bob", "Eve"]))
 scale = Factor((["Eng", "Sec"], ["Hi", "Lo"]))
 
-list(dep)
-list(Product(), (dep, scale))
-list(MergeJoin{(1,2), Product}(Product()), (dep, scale))
+select(Chain(()), (dep, scale), ((1,1), (1,2), (2,2)))
+select(Chain((MergeJoin2((1,2)),)), (dep, scale), ((1,1), (1,2), (2,2)))
+select(Chain((MergeJoin2((1,2)), MergeJoin1((1,)))), (dep, scale), ((1,1), (1,2), (2,2)))
 
+# struct Product end
+
+# function foreach(f, product::Product, factors)
+#     if length(factors) == 0
+#         f(())
+#     else
+#         foreach(factors[1]) do row_left
+#             foreach(product, factors[2:end]) do row_right
+#                 f(tuple(row_left..., row_right...))
+#             end
+#         end
+#     end
+# end   

75a711ec05536de4a54344ba80e1cfa2f0e3c6a7
Jamie Brandon <jamie@scattered-thoughts.net>
2018-07-10 21:32:30 +0100

    GenericJoin in simple interpreter

--- src/first.jl
+++ src/first.jl
@@ -23,63 +23,45 @@ function factor_next(factor::Factor)::Union{Tuple{Any, Factor}, Nothing}
     (value, Factor(factor.columns, factor.column_ix, lohi))
 end
 
-macro factor_next(f)
-    quote
-        n = factor_next($(esc(f)))
-        n == nothing && return
-        n
-    end
+function factor_seek(factor::Factor, value)::Union{Factor, Nothing}
+    lohi = searchsorted(factor.columns[factor.column_ix], value)
+    lohi.start > lohi.stop && return nothing
+    Factor(factor.columns, factor.column_ix, lohi)
 end
 
-struct MergeJoin1{factor_ixes} end
-
-MergeJoin1(factor_ixes) = MergeJoin1{factor_ixes}()
-
-function factors_merge(factors, (a, fa))
-    factors = Any[factors...]
-    factors[a] = fa
-    tuple(factors...)
+function factor_count(factor::Factor)::Int64
+    factor.lohi.stop - factor.lohi.start + 1
 end
 
-function foreach(f, join::MergeJoin1{factor_ixes}, factors) where {factor_ixes}
-    (a,) = factor_ixes
-
-    (va, fa) = factor_first(factors[a])
-
-    while true
-        f(factors_merge(factors, a => fa))
-        (va, fa) = @factor_next(fa)
-    end
+struct GenericJoin
+    factor_ixes
 end
 
-struct MergeJoin2{factor_ixes} end
-
-MergeJoin2(factor_ixes) = MergeJoin2{factor_ixes}()
-
-function factors_merge(factors, (a, fa), (b, fb))
-    factors = Any[factors...]
-    factors[a] = fa
-    factors[b] = fb
-    tuple(factors...)
-end
-
-function foreach(f, join::MergeJoin2{factor_ixes}, factors) where {factor_ixes}
-    (a, b) = factor_ixes
-
-    (va, fa) = factor_first(factors[a])
-    (vb, fb) = factor_first(factors[b])
-
-    while true
-        c = cmp(va, vb)
-        if c == 0
-            f(factors_merge(factors, a => fa, b => fb))
-            (va, fa) = @factor_next(fa)
-            (vb, fb) = @factor_next(fb)
-        elseif c == -1
-            (va, fa) = @factor_next(fa)
-        elseif c == 1
-            (vb, fb) = @factor_next(fb)
+function foreach(f, join::GenericJoin, factors)
+    factors = [factors...]
+    (_, min_ix) = findmin([factor_count(factor) for factor in factors[[join.factor_ixes...]]])
+    (value, min_factor) = factor_first(factors[join.factor_ixes[min_ix]])
+    for (i, factor_ix) in enumerate(join.factor_ixes)
+        if i != min_ix
+            (_, factor) = factor_first(factors[factor_ix])
+            factors[factor_ix] = factor
         end
+    end
+    while true
+        for (i, factor_ix) in enumerate(join.factor_ixes)
+            if i != min_ix
+                factor = factor_seek(factors[factor_ix], value)
+                factor == nothing && @goto next
+                factors[factor_ix] = factor
+            end
+        end
+
+        f(tuple(factors...))
+
+        @label next
+        next = @factor_next(min_factor)
+        next == nothing && return
+        (value, min_factor) = next
     end
 end
 
@@ -114,5 +96,5 @@ dep = Factor((["Eng", "Eng", "Sec"], ["Alice", "Bob", "Eve"]))
 scale = Factor((["Eng", "Sec"], ["Hi", "Lo"]))
 
 select(Chain(()), (dep, scale), ((1,1), (1,2), (2,2)))
-select(Chain((MergeJoin2((1,2)),)), (dep, scale), ((1,1), (1,2), (2,2)))
-select(Chain((MergeJoin2((1,2)), MergeJoin1((1,)))), (dep, scale), ((1,1), (1,2), (2,2)))
+select(Chain((GenericJoin((1,2)),)), (dep, scale), ((1,1), (1,2), (2,2)))
+select(Chain((GenericJoin((1,2)), GenericJoin((1,)))), (dep, scale), ((1,1), (1,2), (2,2)))

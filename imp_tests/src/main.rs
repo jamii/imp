ad2a134ae7f421a20ebdb82a9e21d5c2903d89c5
Jamie Brandon <jamie@scattered-thoughts.net>
2020-01-21 16:06:13 +0000

    Better fuzz repro.

--- imp_tests/src/main.rs
+++ imp_tests/src/main.rs
@@ -1,3 +1,27 @@
+use imp_tests::*;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    imp_tests::fuzz_artifacts()
+    env_logger::init();
+    let args = std::env::args().collect::<Vec<_>>();
+    let args = args.iter().map(|s| &**s).collect::<Vec<_>>();
+    match &*args {
+        [_, "fuzz_parse", path] => {
+            let mut data = vec![];
+            File::open(path).unwrap().read_to_end(&mut data).unwrap();
+            fuzz_parse(&data);
+        }
+        [_, "fuzz_typecheck", path] => {
+            let mut data = vec![];
+            File::open(path).unwrap().read_to_end(&mut data).unwrap();
+            fuzz_typecheck(&data);
+        }
+        [_, "fuzz_eval", path] => {
+            let mut data = vec![];
+            File::open(path).unwrap().read_to_end(&mut data).unwrap();
+            fuzz_eval(&data);
+        }
+        _ => panic!("Unknown args: {:?}", args),
+    }
 }

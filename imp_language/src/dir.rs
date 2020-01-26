9210028be361c999f298ea3a3095bbce2a01f211
Jamie Brandon <jamie@scattered-thoughts.net>
2020-01-26 21:42:13 +0000

    Clean up dir slightly

--- imp_language/src/dir.rs
+++ imp_language/src/dir.rs
@@ -226,144 +226,7 @@ impl Expression {
 
                 let a_slot = a.into_dirs(outer_slot, outer_names, context)?;
 
-                fn apply_to<'a>(
-                    outer_slot: Slot,
-                    outer_names: &[crate::Name],
-                    mut a_slot: Slot,
-                    b: &'a Expression,
-                    context: &DirsContext<'a>,
-                ) -> Result<Slot, String> {
-                    Ok(if !context.type_cache.get(&b).is_function() {
-                        let mut b_slot = b.into_dirs(outer_slot, outer_names, context)?;
-                        let mut a_arity = context.arity(a_slot).unwrap_or(0);
-                        let mut b_arity = context.arity(b_slot).unwrap_or(0);
-                        let outer_arity = outer_names.len();
-                        if a_arity > b_arity {
-                            std::mem::swap(&mut a_slot, &mut b_slot);
-                            std::mem::swap(&mut a_arity, &mut b_arity);
-                        }
-                        context.dir(Dir::Project(
-                            context.dir(Dir::FilterEqual(
-                                context.dir(Dir::Product(vec![a_slot, b_slot])),
-                                (0..a_arity).map(|i| (i, a_arity + i)).collect(),
-                            )),
-                            (0..outer_arity)
-                                .chain(a_arity + a_arity..a_arity + b_arity)
-                                .collect(),
-                        ))
-                    } else {
-                        // if b is a fun, must be either name, abstract or native
-                        match &b {
-                            Name(name) => {
-                                // TODO gross!
-                                let mut env = Environment::new();
-                                std::mem::swap(&mut env, &mut context.env.borrow_mut());
-                                let (_binding_names, binding) = env.lookup(&name).unwrap();
-                                // don't need binding_names because must be subset of outer_names
-                                let slot = match binding {
-                                    Binding::Fun(env, fun) => {
-                                        let mut env = env.clone();
-                                        std::mem::swap(&mut env, &mut context.env.borrow_mut());
-                                        let slot = apply_to(
-                                            outer_slot,
-                                            outer_names,
-                                            a_slot,
-                                            fun,
-                                            context,
-                                        )?;
-                                        std::mem::swap(&mut env, &mut context.env.borrow_mut());
-                                        slot
-                                    }
-                                    _ => unreachable!(),
-                                };
-                                std::mem::swap(&mut env, &mut context.env.borrow_mut());
-                                slot
-                            }
-                            Abstract(name, body) => {
-                                let mut body = &**body;
-                                let mut names = vec![name.clone()];
-                                while let Abstract(name, body2) = body {
-                                    names.push(name.clone());
-                                    body = &**body2;
-                                }
-
-                                // funified!
-                                assert!(!context.type_cache.get(&body).is_function());
-                                if let Option::Some(a_arity) = context.arity(a_slot) {
-                                    assert!(names.len() <= a_arity)
-                                }
-
-                                let new_outer_names = outer_names
-                                    .iter()
-                                    .chain(names.iter())
-                                    .cloned()
-                                    .collect::<Vec<_>>();
-                                let new_outer_slot = context.dir(Dir::Project(
-                                    a_slot,
-                                    (0..new_outer_names.len()).collect(),
-                                ));
-
-                                for name in &names {
-                                    context.env.borrow_mut().bind(
-                                        name.clone(),
-                                        (outer_names.to_vec(), Binding::Scalar),
-                                    );
-                                }
-                                let ab_slot = apply_to(
-                                    new_outer_slot,
-                                    &new_outer_names,
-                                    a_slot,
-                                    body,
-                                    context,
-                                )?;
-                                for _ in &names {
-                                    context.env.borrow_mut().unbind();
-                                }
-
-                                let outer_arity = outer_names.len();
-                                let new_outer_arity = new_outer_names.len();
-                                let ab_arity = context.arity(ab_slot).unwrap_or(0);
-                                let applied = context.dir(Dir::Project(
-                                    ab_slot,
-                                    (0..outer_arity).chain(new_outer_arity..ab_arity).collect(),
-                                ));
-
-                                applied
-                            }
-                            Native(native) => {
-                                let outer_arity = context.arity(outer_slot).unwrap_or(0);
-                                if let Option::Some(a_arity) = context.arity(a_slot) {
-                                    // funified!
-                                    assert_eq!(outer_arity + native.input_arity, a_arity);
-                                }
-                                context.dir(Dir::ApplyNative(a_slot, native.clone()))
-                            }
-                            // TODO gross that we have to repeat this
-                            Let(name, value, body) => {
-                                if context.type_cache.get(value).is_function() {
-                                    let env = (*context.env.borrow()).clone();
-                                    context.env.borrow_mut().bind(
-                                        name.clone(),
-                                        (outer_names.to_vec(), Binding::Fun(env, b)),
-                                    );
-                                } else {
-                                    let value_slot =
-                                        value.into_dirs(outer_slot, outer_names, context)?;
-                                    context.env.borrow_mut().bind(
-                                        name.clone(),
-                                        (outer_names.to_vec(), Binding::Set(value_slot)),
-                                    );
-                                }
-                                let body_slot =
-                                    apply_to(outer_slot, outer_names, a_slot, body, context)?;
-                                context.env.borrow_mut().unbind();
-                                body_slot
-                            }
-                            other => unreachable!("What are this {:?}", other),
-                        }
-                    })
-                }
-                apply_to(outer_slot, outer_names, a_slot, &b, context)?
+                b.apply_to(outer_slot, outer_names, a_slot, context)?
             }
             Native(_native) => {
                 return Err(format!("Bare native!"));
@@ -379,6 +242,131 @@ impl Expression {
                 .and_then(|a| context.arity(outer_slot).map(|b| a + b))
         );
         slot
+    }
+
+    fn apply_to<'a>(
+        &'a self,
+        outer_slot: Slot,
+        outer_names: &[crate::Name],
+        mut a_slot: Slot,
+        context: &DirsContext<'a>,
+    ) -> Result<Slot, String> {
+        use Expression::*;
+        let b = self;
+        Ok(if !context.type_cache.get(&b).is_function() {
+            let mut b_slot = b.into_dirs(outer_slot, outer_names, context)?;
+            let mut a_arity = context.arity(a_slot).unwrap_or(0);
+            let mut b_arity = context.arity(b_slot).unwrap_or(0);
+            let outer_arity = outer_names.len();
+            if a_arity > b_arity {
+                std::mem::swap(&mut a_slot, &mut b_slot);
+                std::mem::swap(&mut a_arity, &mut b_arity);
+            }
+            context.dir(Dir::Project(
+                context.dir(Dir::FilterEqual(
+                    context.dir(Dir::Product(vec![a_slot, b_slot])),
+                    (0..a_arity).map(|i| (i, a_arity + i)).collect(),
+                )),
+                (0..outer_arity)
+                    .chain(a_arity + a_arity..a_arity + b_arity)
+                    .collect(),
+            ))
+        } else {
+            // if b is a fun, must be either name, abstract or native
+            match &b {
+                Name(name) => {
+                    // TODO gross!
+                    let mut env = Environment::new();
+                    std::mem::swap(&mut env, &mut context.env.borrow_mut());
+                    let (_binding_names, binding) = env.lookup(&name).unwrap();
+                    // don't need binding_names because must be subset of outer_names
+                    let slot = match binding {
+                        Binding::Fun(env, fun) => {
+                            let mut env = env.clone();
+                            std::mem::swap(&mut env, &mut context.env.borrow_mut());
+                            let slot = fun.apply_to(outer_slot, outer_names, a_slot, context)?;
+                            std::mem::swap(&mut env, &mut context.env.borrow_mut());
+                            slot
+                        }
+                        _ => unreachable!(),
+                    };
+                    std::mem::swap(&mut env, &mut context.env.borrow_mut());
+                    slot
+                }
+                Abstract(name, body) => {
+                    let mut body = &**body;
+                    let mut names = vec![name.clone()];
+                    while let Abstract(name, body2) = body {
+                        names.push(name.clone());
+                        body = &**body2;
+                    }
+
+                    // funified!
+                    assert!(!context.type_cache.get(&body).is_function());
+                    if let Option::Some(a_arity) = context.arity(a_slot) {
+                        assert!(names.len() <= a_arity)
+                    }
+
+                    let new_outer_names = outer_names
+                        .iter()
+                        .chain(names.iter())
+                        .cloned()
+                        .collect::<Vec<_>>();
+                    let new_outer_slot =
+                        context.dir(Dir::Project(a_slot, (0..new_outer_names.len()).collect()));
+
+                    for name in &names {
+                        context
+                            .env
+                            .borrow_mut()
+                            .bind(name.clone(), (outer_names.to_vec(), Binding::Scalar));
+                    }
+                    let ab_slot =
+                        body.apply_to(new_outer_slot, &new_outer_names, a_slot, context)?;
+                    for _ in &names {
+                        context.env.borrow_mut().unbind();
+                    }
+
+                    let outer_arity = outer_names.len();
+                    let new_outer_arity = new_outer_names.len();
+                    let ab_arity = context.arity(ab_slot).unwrap_or(0);
+                    let applied = context.dir(Dir::Project(
+                        ab_slot,
+                        (0..outer_arity).chain(new_outer_arity..ab_arity).collect(),
+                    ));
+
+                    applied
+                }
+                Native(native) => {
+                    let outer_arity = context.arity(outer_slot).unwrap_or(0);
+                    if let Option::Some(a_arity) = context.arity(a_slot) {
+                        // funified!
+                        assert_eq!(outer_arity + native.input_arity, a_arity);
+                    }
+                    context.dir(Dir::ApplyNative(a_slot, native.clone()))
+                }
+                // TODO gross that we have to repeat this
+                Let(name, value, body) => {
+                    if context.type_cache.get(value).is_function() {
+                        let env = (*context.env.borrow()).clone();
+                        context
+                            .env
+                            .borrow_mut()
+                            .bind(name.clone(), (outer_names.to_vec(), Binding::Fun(env, b)));
+                    } else {
+                        let value_slot = value.into_dirs(outer_slot, outer_names, context)?;
+                        context.env.borrow_mut().bind(
+                            name.clone(),
+                            (outer_names.to_vec(), Binding::Set(value_slot)),
+                        );
+                    }
+                    let body_slot = body.apply_to(outer_slot, outer_names, a_slot, context)?;
+                    context.env.borrow_mut().unbind();
+                    body_slot
+                }
+                other => unreachable!("What are this {:?}", other),
+            }
+        })
     }
 }
 
@@ -446,5 +434,3 @@ impl Dirs {
         Ok(data[slot].clone())
     }
 }
-
-// TODO combine outer into apply_to?

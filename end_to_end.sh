d56d4033a98a0e177c7873206b9a589cd1bfe205
Jamie Brandon <jamie@scattered-thoughts.net>
2020-05-31 20:06:57 -0700

    Don't embed test file - requires recompilation

--- end_to_end.sh
+++ end_to_end.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-zig run ./test/end_to_end.zig --main-pkg-path ./ -lc $@
+ARGS=$@
+if [ -z $ARGS ] ; then
+    ARGS=$(ls test/*.test)
+fi
+
+zig run ./test/end_to_end.zig --main-pkg-path ./ -lc -- $ARGS
--- test/end_to_end.zig
+++ test/end_to_end.zig
@@ -7,43 +7,68 @@ pub fn main() anyerror ! void {
 
     var num_tests: usize = 0;
     var num_failed: usize = 0;
-    const cases = @embedFile("./end_to_end.test");
 
-    var cases_iter = std.mem.split(cases, "\n\n");
-    while (cases_iter.next()) |case| {
-        var case_iter = std.mem.split(case, "---");
-        const input = std.mem.trim(u8, case_iter.next().?, "\n ");
-        const expected = std.mem.trim(u8, case_iter.next().?, "\n ");
-        expect(case_iter.next() == null);
+    var args = std.process.args();
+    // arg 0 is executable
+    _ = try args.next(allocator).?;
+    while (args.next(allocator)) |arg| {
+        const filename = try arg;
 
-        var arena = ArenaAllocator.init(allocator);
-        defer arena.deinit();
+        var file = if (std.mem.eql(u8, filename, "-"))
+            std.io.getStdIn()
+            else
+            try std.fs.cwd().openFile(filename, .{});
 
-        var error_info: ?imp.lang.InterpretErrorInfo = null;
-        const result = imp.lang.interpret(&arena, input, &error_info);
-
-        var bytes = ArrayList(u8).init(allocator);
-        defer bytes.deinit();
-        const out_stream = bytes.outStream();
-        if (result) |type_and_set| {
-            try type_and_set.dumpInto(allocator, out_stream);
-        } else |err| {
-            try imp.lang.InterpretErrorInfo.dumpInto(error_info, err, out_stream);
+        // TODO can't use readFileAlloc on stdin
+        var cases = ArrayList(u8).init(allocator);
+        defer cases.deinit();
+        {
+            const chunk_size = 1024;
+            var buf = try allocator.alloc(u8, chunk_size);
+            defer allocator.free(buf);
+            while (true) {
+                const len = try file.readAll(buf);
+                try cases.appendSlice(buf[0..len]);
+                if (len < chunk_size) break;
+            }
         }
-        const found = std.mem.trim(u8, bytes.items, "\n ");
 
-        num_tests += 1;
-        if (!std.mem.eql(u8, expected, found)) {
-            num_failed += 1;
-            warn("Source:\n{}\n\nExpected:\n{}\n\nFound:\n{}\n---\n\n", .{input, expected, found});
+        var cases_iter = std.mem.split(cases.items, "\n\n");
+        while (cases_iter.next()) |case| {
+            var case_iter = std.mem.split(case, "---");
+            const input = std.mem.trim(u8, case_iter.next().?, "\n ");
+            const expected = std.mem.trim(u8, case_iter.next().?, "\n ");
+            expect(case_iter.next() == null);
+
+            var arena = ArenaAllocator.init(allocator);
+            defer arena.deinit();
+
+            var error_info: ?imp.lang.InterpretErrorInfo = null;
+            const result = imp.lang.interpret(&arena, input, &error_info);
+
+            var bytes = ArrayList(u8).init(allocator);
+            defer bytes.deinit();
+            const out_stream = bytes.outStream();
+            if (result) |type_and_set| {
+                try type_and_set.dumpInto(allocator, out_stream);
+            } else |err| {
+                try imp.lang.InterpretErrorInfo.dumpInto(error_info, err, out_stream);
+            }
+            const found = std.mem.trim(u8, bytes.items, "\n ");
+
+            num_tests += 1;
+            if (!std.mem.eql(u8, expected, found)) {
+                num_failed += 1;
+                warn("Filename:\n{}\nSource:\n{}\n\nExpected:\n{}\n\nFound:\n{}\n---\n\n", .{filename, input, expected, found});
+            }
         }
-    }
 
-    if (num_failed > 0) {
-        warn("{}/{} tests failed!\n", .{num_failed, num_tests});
-        std.os.exit(1);
-    } else {
-        warn("All {} tests passed\n", .{num_tests});
-        std.os.exit(0);
+        if (num_failed > 0) {
+            warn("{}/{} tests failed!\n", .{num_failed, num_tests});
+            std.os.exit(1);
+        } else {
+            warn("All {} tests passed\n", .{num_tests});
+            std.os.exit(0);
+        }
     }
 }
